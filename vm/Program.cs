4ef92b083b20a2b8116ff096f41b14112fa9736a
Yuuki Wesp <ls-micro@ya.ru>
2019-05-29 16:36:46 +0300

    impl stage chars, merge and print to console

--- vm/Program.cs
+++ vm/Program.cs
@@ -41,6 +41,22 @@
                 0x3120000, // swipe &r1 &r2
                 0xE000103, // print &r1
                 0xE000203, // print &r2
+                //core.Compile(0xF, 0x0, 0x0, 0xE, ((byte)'H' & 0xF0) >> 4, ((byte)'H' & 0xF) >> 0, 0xC),
+                0xF00E48C, // push 'H'
+                0xF00E45C, // push 'E'
+                0xF00E4CC, // push 'L'
+                0xF00E4CC, // push 'L'
+                0xF00E4FC, // push 'O'
+                0xF00E20C, // push ' '
+                0xF00E57C, // push 'W'
+                0xF00E4FC, // push 'O'
+                0xF00E52C, // push 'R'
+                0xF00E4CC, // push 'L'
+                0xF00E44C, // push 'D'
+                0xF00E21C, // push '!'
+                0xF00E0AC, // push '\n'
+                0xF00E00F, // pop, and merge
+                //core.Compile(0xF, 0x0, 0x0, 0xE, 0x0, 0x0, 0xF),
                 0xDEAD     // halt
             });
 
@@ -67,15 +83,35 @@
             stack.Eval();
             await Task.CompletedTask;
         }
+
+        public uint Compile(uint opcode, uint r1 = 0, uint r2 = 0, uint r3 = 0, uint u1 = 0, uint u2 = 0, uint x1 = 0)
+        {
+            return (opcode << 24) | (r1 << 20) | (r2 << 16) | (r3 << 12) | (u1 << 8) | (u2 << 4) | x1;
+        }
     }
     public unsafe class Stack
     {
         public ulong pc { get; set; } = 0;
 
+        /// <summary>
+        /// base register cell
+        /// </summary>
         public ulong r1, r2, r3;
+        /// <summary>
+        /// value register cell
+        /// </summary>
         public ulong u1, u2;
+        /// <summary>
+        /// magic cell
+        /// </summary>
         public ulong x1;
+        /// <summary>
+        /// id
+        /// </summary>
         public ulong instructionID;
+        /// <summary>
+        /// last <see cref="r1"/>
+        /// </summary>
         public ulong prev;
 
         public ulong[] regs = new ulong[16];
@@ -87,7 +123,7 @@
 
         public uint Fetch() => program.ElementAt((int)pc++);
 
-
+        public string pX = "";
         public void Eval()
         {
             Print();
@@ -107,6 +143,12 @@
                 case 0x0:
                 case 0xD when r1 == 0xE && r2 == 0xA && r3 == 0xD:
                     WriteLine("HALT");
+                    break;
+                case 0xF when x1 == 0xC && r3 == 0xE:
+                    WriteLine($"push ::pX ::0x{u1:X} ::&0x{u2:X}");
+                    break;
+                case 0xF when x1 == 0xF && r3 == 0xE:
+                    WriteLine($"pop, print ::pX");
                     break;
                 case 0x3:
                     WriteLine($"swap ::0x{r1:X4} ::0x{r2:X4}");
@@ -139,17 +181,48 @@
                 case 0x2:
                     regs[r1] = regs[r2] + regs[r3];
                     break;
+                case 0x3:
+                    regs[r1] ^= regs[r2];
+                    regs[r2] = regs[r1] ^ regs[r2];
+                    regs[r1] ^= regs[r2];
+                    break;
+                case 0x4:
+                    regs[r1] = regs[r2] - regs[r3];
+                    break;
+                case 0x5:
+                    regs[r1] = regs[r2] * regs[r3];
+                    break;
+                case 0x6:
+                    regs[r1] = regs[r2] / regs[r3];
+                    break;
+                case 0x7:
+                    regs[r1] = (uint)Math.Pow(regs[r2], regs[r3]);
+                    break;
+                case 0x8 when u2 == 0xF:
+                    pc = regs[r1];
+                    break;
                 case 0x0:
                 case 0xD when r1 == 0xE && r2 == 0xA && r3 == 0xD:
                     halt = 1;
                     break;
-                case 0x3:
-                    regs[r1] ^= regs[r2];
-                    regs[r2]  = regs[r1] ^ regs[r2];
-                    regs[r1] ^= regs[r2];
-                    break;
-                case 0x4: break;
                 case 0xA: break;
+
+                case 0xF when x1 == 0xC && r3 == 0xE:
+                    pX += (char)(u1 << 4 | u2);
+                    break;
+                case 0xF when x1 == 0xF && r3 == 0xE:
+                    ForegroundColor = ConsoleColor.Red;
+                    Write(pX);
+                    pX = "";
+                    ForegroundColor = ConsoleColor.White;
+                    break;
+                case 0xF when x1 == 0x1:
+                case 0xF when x1 == 0x2:
+                case 0xF when x1 == 0x3:
+                case 0xE when x1 == 0x1:
+                case 0xE when x1 == 0x2:
+                case 0xE when x1 == 0x3:
+                    break;
             }
             prev = r1;
         }
@@ -169,6 +242,11 @@
             u1            = (mem & 0xF00    ) >> 8 ;
             u2            = (mem & 0xF0     ) >> 4 ;
             x1            = (mem & 0xF      )      ;
+        }
+
+        public ulong Degrade()
+        {
+            return (instructionID << 24) | (r1 << 20) | (r2 << 16) | (r3 << 12) | (u1 << 8) | (u2 << 4) | x1;
         }
     }
 }
