cbd9dbca569527e57645d9e9342900c4f6aa702c
Yuuki Wesp <ls-micro@ya.ru>
2019-05-30 06:56:30 +0300

    add instruction class

---
+++ vm/models/Instruction.cs
@@ -0,0 +1,19 @@
+﻿namespace vm.models
+{
+    public abstract class Instruction
+    {
+        public InsID ID { get; }
+        public short OPCode { get; }
+
+        protected Instruction(InsID id, short opCode)
+        {
+            ID = id;
+            OPCode = opCode;
+        }
+
+        public abstract ulong Assembly();
+
+        public static implicit operator ulong(Instruction i) => i.Assembly();
+        public static implicit operator uint(Instruction i) => (uint)i.Assembly();
+    }
+}
