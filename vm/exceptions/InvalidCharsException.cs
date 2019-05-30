f69efb786523a56172b922448371a3e8fc8d9945
Yuuki Wesp <ls-micro@ya.ru>
2019-05-30 06:56:08 +0300

    add InvalidCharsException

---
+++ vm/exceptions/InvalidCharsException.cs
@@ -0,0 +1,9 @@
+﻿namespace vm.exceptions
+{
+    using System;
+
+    public class InvalidCharsException : Exception
+    {
+        public InvalidCharsException(string msg) : base(msg) { }
+    }
+}
