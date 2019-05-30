880126f78b632762e261936c52249cfdd90bf61b
Yuuki Wesp <ls-micro@ya.ru>
2019-05-30 06:56:16 +0300

    add Instruction list

---
+++ vm/models/InsID.cs
@@ -0,0 +1,23 @@
+﻿namespace vm.models
+{
+    public enum InsID : short
+    {
+        warm,
+        loadi,
+        add,
+        sub,
+        div,
+        mul,
+        pow,
+        push_a,
+        push_d,
+        swap,
+
+        ref_t,
+        jump_t,
+
+        mov_d,
+
+        halt
+    }
+}
