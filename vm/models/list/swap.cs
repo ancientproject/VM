9bd39abe82bafd577369165a2506df76bd64724b
Yuuki Wesp <ls-micro@ya.ru>
2019-05-30 06:57:37 +0300

    add swap instruction

---
+++ vm/models/list/swap.cs
@@ -0,0 +1,18 @@
+﻿namespace vm.models.list
+{
+    public class swap : Instruction
+
+    {
+        private readonly short _index1;
+        private readonly short _index2;
+
+        public swap(short index1, short index2) : base(InsID.swap, 0x3)
+        {
+            _index1 = index1;
+            _index2 = index2;
+        }
+
+        public override ulong Assembly()
+            => (ulong)((OPCode << 24) | (_index1 << 20) | (_index2 << 16) | (0 << 12) | (0 << 8) | (0 << 4) | 0);
+    }
+}
