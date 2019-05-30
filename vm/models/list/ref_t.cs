ff5cec27a3a0a4e4cbb79d09edd67663060fa3d3
Yuuki Wesp <ls-micro@ya.ru>
2019-05-30 06:57:28 +0300

    add ref_t instruction

---
+++ vm/models/list/ref_t.cs
@@ -0,0 +1,12 @@
+﻿namespace vm.models.list
+{
+    public class ref_t : Instruction
+    {
+        private readonly ushort _cell;
+
+        public ref_t(ushort cell) : base(InsID.ref_t, 0x8) => _cell = cell;
+
+        public override ulong Assembly()
+            => (ulong)((OPCode << 24) | (_cell << 20) | (0 << 16) | (0 << 12) | (0 << 8) | (0xC << 4) | 0);
+    }
+}
