5585200de02a00a2f72b66b263e2f504f14796bd
Yuuki Wesp <ls-micro@ya.ru>
2019-05-30 06:57:08 +0300

    add jump_t instruction

---
+++ vm/models/list/jump_t.cs
@@ -0,0 +1,12 @@
+﻿namespace vm.models.list
+{
+    public class jump_t : Instruction
+    {
+        private readonly ushort _cell;
+
+        public jump_t(ushort cell) : base(InsID.jump_t, 0x8) => _cell = cell;
+
+        public override ulong Assembly()
+            => (ulong)((OPCode << 24) | (_cell << 20) | (0 << 16) | (0 << 12) | (0 << 8) | (0xF << 4) | 0);
+    }
+}
