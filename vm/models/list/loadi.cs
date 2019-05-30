e1bb31e0a4e491910382990b59c85ee43c3d0d45
Yuuki Wesp <ls-micro@ya.ru>
2019-05-30 06:57:15 +0300

    add loadi instruction

---
+++ vm/models/list/loadi.cs
@@ -0,0 +1,23 @@
+﻿namespace vm.models.list
+{
+    public class loadi : Instruction
+    {
+        private readonly ushort _index;
+        private readonly ushort _value;
+
+        public loadi(ushort index, ushort value) : base(InsID.loadi, 0x1)
+        {
+            _index = index;
+            _value = value;
+        }
+
+        public override ulong Assembly()
+        {
+            var u1 = (_value & 0xF0) >> 4;
+            var u2 = (_value & 0xF);
+            return (ulong)((OPCode << 24) | (_index << 20) | (0 << 16) | (0 << 12) | (u1 << 8) | (u2 << 4) | 0);
+        }
+
+        
+    }
+}
