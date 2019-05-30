7c4ba036461d6f6580ccc1be0febb05340fe0169
Yuuki Wesp <ls-micro@ya.ru>
2019-05-30 04:26:14 +0300

    add attributes

---
+++ vm/dev/Internal/PropAddressAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace vm.dev.Internal
+{
+    using System;
+    [AttributeUsage(AttributeTargets.Property)]
+    public class PropAddressAttribute : Attribute
+    {
+        public short Address { get; }
+        public PropAddressAttribute(short address) => Address = address;
+    }
+}
---
+++ vm/dev/Internal/StringAddress.cs
@@ -0,0 +1,17 @@
+﻿namespace vm.dev.Internal
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class StringAddress : Attribute
+    {
+        public readonly short StartAddress;
+        public readonly short ContentAddress;
+
+        public StringAddress(short startAddress, short contentAddress)
+        {
+            StartAddress = startAddress;
+            ContentAddress = contentAddress;
+        }
+    }
+}
