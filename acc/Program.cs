169f79c4f6fd4ed3ee2e44c87f3cd03aed25a44a
Yuuki Wesp <ls-micro@ya.ru>
2019-05-31 01:41:51 +0300

    add compiler

---
+++ acc/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace flame.compiler
+{
+    using System.Drawing;
+    using System.Linq;
+    using static TrueColorConsole.VTConsole;
+    using static _term;
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Enable();
+            CursorSetVisibility(false);
+            CursorSetBlinking(false);
+
+            WriteLine($"Flame Assembler Compiler version 0.0.0.0 (default)", Color.Gray);
+            WriteLine($"Copyright (C) Yuuki Wesp.\n\n", Color.Gray);
+
+            if (!args.Any())
+            {
+                Fatal($"FC0004", "No source files specified.");
+                return;
+            }
+        }
+    }
+}
---
+++ acc/_term.cs
@@ -0,0 +1,43 @@
+﻿namespace flame.compiler
+{
+    using System;
+    using System.Drawing;
+    using static TrueColorConsole.VTConsole;
+    internal class _term
+    {
+        private static readonly object Guarder = new object();
+
+        public static void Warn(string message) => Warn("", message);
+
+        public static void Warn(string keyCode, string message)
+        {
+            lock (Guarder)
+            {
+                if (!IsSupported || !IsEnabled)
+                {
+                    Console.WriteLine($"warning {keyCode}: {message}");
+                    return;
+                }
+                Write("warning ", Color.Orange);
+                Write(keyCode, Color.Orange);
+                Write(": ", Color.White);
+                Write(message, Color.White);
+            }
+        }
+        public static void Error(string keyCode, string message)
+        {
+            lock (Guarder)
+            {
+                if (!IsSupported || !IsEnabled)
+                {
+                    Console.WriteLine($"error {keyCode}: {message}");
+                    return;
+                }
+                Write("warning ", Color.Red);
+                Write(keyCode, Color.Red);
+                Write(": ", Color.White);
+                Write(message, Color.White);
+            }
+        }
+    }
+}
