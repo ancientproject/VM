e75b1919c11d0cd545f48ca1fa6fc549725d3863
Yuuki Wesp <ls-micro@ya.ru>
2019-05-30 06:56:58 +0300

    add halt instruction

---
+++ vm/models/list/halt.cs
@@ -0,0 +1,9 @@
+﻿namespace vm.models.list
+{
+    public class halt : Instruction
+    {
+        public halt() : base(InsID.halt, 0xD) { }
+        public override ulong Assembly()
+            => (ulong)((OPCode << 24) | (0xE << 20) | (0xA << 16) | (0xD << 12) | (0 << 8) | (0 << 4) | 0);
+    }
+}
