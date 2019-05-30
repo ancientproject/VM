032f28fd4788e19bfd194d5fe15dfa5425c9b36f
Yuuki Wesp <ls-micro@ya.ru>
2019-05-30 04:25:55 +0300

    add corrupted device

---
+++ vm/dev/CorruptedDevice.cs
@@ -0,0 +1,29 @@
+﻿namespace vm.dev
+{
+    using exceptions;
+
+    public class CorruptedDevice : IDevice
+    {
+        public string Name { get; }
+        public short StartAddress { get; }
+        public void write(short address, int data)
+        {
+            throw new CorruptedMemoryException();
+        }
+
+        public int read(short address)
+        {
+            throw new CorruptedMemoryException();
+        }
+
+        public void Init()
+        {
+            throw new CorruptedMemoryException();
+        }
+
+        public void Shutdown()
+        {
+            throw new CorruptedMemoryException();
+        }
+    }
+}
