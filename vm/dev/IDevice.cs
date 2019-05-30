d669a95047397938b0dc7111ba1dcbf022d0b1b9
Yuuki Wesp <ls-micro@ya.ru>
2019-05-30 04:26:05 +0300

    add IDevice

---
+++ vm/dev/IDevice.cs
@@ -0,0 +1,16 @@
+﻿namespace vm.dev
+{
+    using System.Threading.Tasks;
+
+    public interface IDevice
+    {
+        string Name { get; }
+        short StartAddress { get; }
+        void write(short address, int data);
+        int read(short address);
+
+
+        void Init();
+        void Shutdown();
+    }
+}
