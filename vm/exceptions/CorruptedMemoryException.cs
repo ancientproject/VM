7998b5238d3f90ac1df68b0fa76c33569eaf4e46
Yuuki Wesp <ls-micro@ya.ru>
2019-05-30 04:26:28 +0300

    add converter and exp

---
+++ vm/exceptions/CorruptedMemoryException.cs
@@ -0,0 +1,10 @@
+﻿namespace vm.exceptions
+{
+    using System;
+
+    public class CorruptedMemoryException : Exception
+    {
+        public CorruptedMemoryException() { }
+        public CorruptedMemoryException(string msg) : base(msg) { }
+    }
+}
