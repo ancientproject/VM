03e72014106f71546a499ac677eda249146007e5
Yuuki Wesp <ls-micro@ya.ru>
2019-06-02 03:11:35 +0300

    add warning enum

---
+++ acc/Emit/Warning.cs
@@ -0,0 +1,18 @@
+﻿namespace flame.compiler.emit
+{
+    using System;
+
+    public enum Warning
+    {
+        Undefined = -1,
+        InternalError = 1,  
+        NoSource = 2,
+        CouldNotWrite = 3,
+        OutFileNotSpecified = 4,
+        SourceFileNotFound = 5,
+    }
+    public static class WarningEx
+    {
+        public static string Format(this Warning w) => $"FC{(int)w:####}";
+    }
+}
