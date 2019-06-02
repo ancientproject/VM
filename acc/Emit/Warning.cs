6998d3b53bb07cc6764c6e3dea49cedd25602e00
Yuuki Wesp <ls-micro@ya.ru>
2019-06-02 16:46:09 +0300

    upd

--- acc/Emit/Warning.cs
+++ acc/Emit/Warning.cs
@@ -1,6 +1,8 @@
 ﻿namespace flame.compiler.emit
 {
     using System;
+    using System.Linq;
+    using Sprache;
 
     public enum Warning
     {
@@ -10,9 +12,17 @@
         CouldNotWrite = 3,
         OutFileNotSpecified = 4,
         SourceFileNotFound = 5,
+        UnexpectedToken = 6
     }
     public static class WarningEx
     {
-        public static string Format(this Warning w) => $"FC{(int)w:00000}";
+        public static string Format(this Warning w) => $"FC{(int)w:0000}";
+
+        public static Warning getWarningCode<T>(this IResult<T> s)
+        {
+            var expect = s.Expectations.First();
+            // TODO
+            return Warning.UnexpectedToken;
+        }
     }
 }
