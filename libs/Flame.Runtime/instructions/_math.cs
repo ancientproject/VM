4ae5a8cb4802e13396a4ebfeac1c65b669fc6930
Yuuki Wesp <ls-micro@ya.ru>
2019-06-03 06:56:29 +0300

    add sqrt token

--- libs/Flame.Runtime/instructions/_math.cs
+++ libs/Flame.Runtime/instructions/_math.cs
@@ -2,34 +2,47 @@
 {
     public class sum : _math
     {
-        public sum(ushort cell, ushort ref1, ushort ref2) : base(cell, ref1, ref2, InsID.add) { }
+        public sum(short cell, short ref1, short ref2) : base(cell, ref1, ref2, InsID.add) { }
     }
     public class sub : _math
     {
-        public sub(ushort cell, ushort ref1, ushort ref2) : base(cell, ref1, ref2, InsID.sub) { }
+        public sub(short cell, short ref1, short ref2) : base(cell, ref1, ref2, InsID.sub) { }
     }
     public class div : _math
     {
-        public div(ushort cell, ushort ref1, ushort ref2) : base(cell, ref1, ref2, InsID.div) { }
+        public div(short cell, short ref1, short ref2) : base(cell, ref1, ref2, InsID.div) { }
     }
     public class mul : _math
     {
-        public mul(ushort cell, ushort ref1, ushort ref2) : base(cell, ref1, ref2, InsID.mul) { }
+        public mul(short cell, short ref1, short ref2) : base(cell, ref1, ref2, InsID.mul) { }
     }
 
     public class pow : _math
     {
-        public pow(ushort cell, ushort ref1, ushort ref2) : base(cell, ref1, ref2, InsID.pow) { }
+        public pow(short cell, short ref1, short ref2) : base(cell, ref1, ref2, InsID.pow) { }
     }
+    public class sqrt : Instruction
+    {
+        private readonly short _cell;
+        private readonly short _ref1;
 
+        public sqrt(short cell, short ref1) : base(InsID.sqrt)
+        {
+            _cell = cell;
+            _ref1 = ref1;
+        }
+
+        public override ulong Assembly()
+            => (ulong)((OPCode << 24) | (_cell << 20) | (_ref1 << 16) | (0 << 12) | (0 << 8) | (0xA << 4) | 0);
+    }
 
     public abstract class _math : Instruction
     {
-        private readonly ushort _cell;
-        private readonly ushort _ref1;
-        private readonly ushort _ref2;
+        private readonly short _cell;
+        private readonly short _ref1;
+        private readonly short _ref2;
 
-        protected _math(ushort cell, ushort ref1, ushort ref2, InsID id) : base(id)
+        protected _math(short cell, short ref1, short ref2, InsID id) : base(id)
         {
             _cell = cell;
             _ref1 = ref1;
