b443f50267de321d87b9f5d3f9fbf2eb633761e4
Yuuki Wesp <ls-micro@ya.ru>
2019-06-03 06:55:46 +0300

    add tramsform token

---
+++ acc/Tokens/TransformPushJ.cs
@@ -0,0 +1,13 @@
+﻿namespace flame.compiler.tokens
+{
+    using System.Linq;
+    using runtime;
+
+    public class TransformPushJ : TransformationContext
+    {
+        public TransformPushJ(string value, short cellDev, short ActionDev)
+        {
+            Instructions = value.Select(x => new push_a(cellDev, ActionDev, (short) x)).Cast<Instruction>().ToArray();
+        }
+    }
+}
---
+++ acc/Tokens/TransformationContext.cs
@@ -0,0 +1,11 @@
+﻿namespace flame.compiler.tokens
+{
+    using runtime;
+    using Sprache;
+
+    public class TransformationContext : IInputToken
+    {
+        public Instruction[] Instructions { get; set; }
+        public Position InputPosition { get; set; }
+    }
+}
