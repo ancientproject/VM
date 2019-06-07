7a7b3b36aade485c15ccf37a29fa1dff48c1b34e
Yuuki Wesp <ls-micro@ya.ru>
2019-06-08 02:53:39 +0300

    fix test

--- test/acc_test/InstructionTest.cs
+++ test/acc_test/InstructionTest.cs
@@ -12,12 +12,8 @@ namespace flame.runtime.compiler.test
         public void PushJ(string code)
         {
             var result = SyntaxStorage.PushJ.End().Parse(code);
-
-            if (result is TransformationContext i)
-            {
-                Assert.Equal(4, i.Instructions.Length);
-            }
-            else Assert.True(false);
+            Assert.True(result is TransformationContext);
+            Assert.Equal(4, (result as TransformationContext).Instructions.Length);
         }
 
         [Theory]
@@ -25,70 +21,65 @@ namespace flame.runtime.compiler.test
         [InlineData(".push_a &(0x0) &(0xC) |> @char_t('ÿ')")]
         public void PushA(string code)
         {
-            var result = SyntaxStorage.PushA.End().Parse(code) as InstructionExpression;
+            var result = SyntaxStorage.PushA.End().Parse(code);
 
-            if (result.Instruction is push_a i)
+            if(result is InstructionExpression exp && exp.Instruction is push_a i)
             {
                 Assert.Equal(InsID.push_a, i.ID);
                 Assert.Equal(0x0, i._addressBus);
                 Assert.Equal(0xC, i._addressDev);
                 Assert.Equal(0xFF, i._value);
             }
-            else Assert.True(false);
         }
         [Theory]
         [InlineData(".loadi &(0x0) <| $(0xF)")]
         public void LoadI(string code)
         {
-            var result = SyntaxStorage.LoadI.End().Parse(code) as InstructionExpression;
+            var result = SyntaxStorage.LoadI.End().Parse(code);
 
-            if (result.Instruction is loadi i)
+            if(result is InstructionExpression exp && exp.Instruction is loadi i)
             {
                 Assert.Equal(InsID.loadi, i.ID);
                 Assert.Equal(0x0, i._index);
                 Assert.Equal(0xF, i._value);
             }
-            else Assert.True(false);
         }
         [Theory]
         [InlineData(".jump_t &(0x0)")]
         public void JumpT(string code)
         {
-            var result = SyntaxStorage.JumpT.End().Parse(code) as InstructionExpression;
+            var result = SyntaxStorage.JumpT.End().Parse(code);
 
-            if (result.Instruction is jump_t i)
+            if(result is InstructionExpression exp && exp.Instruction is jump_t i)
             {
                 Assert.Equal(InsID.jump_t, i.ID);
                 Assert.Equal(0x0, i._cell);
             }
-            else Assert.True(false);
         }
         [Theory]
         [InlineData(".ref_t &(0x0)")]
         public void RefT(string code)
         {
-            var result = SyntaxStorage.RefT.End().Parse(code) as InstructionExpression;
+            var result = SyntaxStorage.RefT.End().Parse(code);
 
-            if (result.Instruction is ref_t i)
+            if(result is InstructionExpression exp && exp.Instruction is ref_t i)
             {
                 Assert.Equal(InsID.ref_t, i.ID);
                 Assert.Equal(0x0, i._cell);
             }
-            else Assert.True(false);
         }
         [Theory]
         [InlineData(".swap &(0x0) &(0xF)")]
         public void Swap(string code)
         {
-            var result = SyntaxStorage.SwapToken.End().Parse(code) as InstructionExpression;
+            var result = SyntaxStorage.SwapToken.End().Parse(code);
 
-            if (result.Instruction is swap i)
+            if(result is InstructionExpression exp && exp.Instruction is swap i)
             {
                 Assert.Equal(InsID.swap, i.ID);
                 Assert.Equal(0x0, i._index1);
                 Assert.Equal(0xF, i._index2);
             }
-            else Assert.True(false);
         }
         [Theory]
         [InlineData(".halt")]
@@ -98,7 +89,6 @@ namespace flame.runtime.compiler.test
 
             if (result.Instruction is halt i)
                 Assert.Equal(InsID.halt, i.ID);
-            else Assert.True(false);
         }
         [Theory]
         [InlineData(".warm")]
@@ -108,7 +98,6 @@ namespace flame.runtime.compiler.test
 
             if (result.Instruction is warm i)
                 Assert.Equal(InsID.warm, i.ID);
-            else Assert.True(false);
         }
     }
 }
