1108708e301706ffda91a4489d75b2a85187e804
Yuuki Wesp <ls-micro@ya.ru>
2019-06-11 01:55:28 +0300

    remove trash

--- acc/Tokens/InstructionExpression.cs
+++ acc/Tokens/InstructionExpression.cs
@@ -5,11 +5,9 @@
 
     public class InstructionExpression : IInputToken
     {
-        private readonly string _failedError;
         public Position InputPosition { get; set; }
         public Instruction Instruction { get; set; }
 
         public InstructionExpression(Instruction ins) => Instruction = ins;
-        public InstructionExpression(string failedError) => _failedError = failedError;
     }
 }
