db7ca49902067d771f39c00f8681084055fcf507
Yuuki Wesp <ls-micro@ya.ru>
2019-06-13 05:46:45 +0300

    add halt action to cpu

--- vm/component/CPU.cs
+++ vm/component/CPU.cs
@@ -2,7 +2,8 @@
 {
     using System;
     using System.Threading.Tasks;
-
+    using flame.runtime;
+    using static System.Console;
     public class CPU
     {
         private readonly Bus _bus;
@@ -21,9 +22,43 @@
             await Task.CompletedTask;
         }
 
-        public uint Compile(uint opcode, uint r1 = 0, uint r2 = 0, uint r3 = 0, uint u1 = 0, uint u2 = 0, uint x1 = 0)
+        public void Halt(byte code)
         {
-            return (opcode << 0x18) | (r1 << 0x14) | (r2 << 0x10) | (r3 << 0xC) | (u1 << 0x8) | (u2 << 4) | x1;
+            _bus.State.halt = 1;
+            switch (code)
+            {
+                case 0x0:
+                    Error("HALT");
+                    break;
+                case 0x1:
+                    Error("HALT: Bootable sector not found.");
+                    break;
+                case 0xC:
+                    Error("HALT: Divide by zero.");
+                    break;
+                case 0xB:
+                    Error("HALT: Divide by zero.");
+                    break;
+                case 0xF:
+                    Error($"HALT: Corrupted memory.");
+                    break;
+                default:
+                    Error($"HALT: Unknown state 0x{code:X}");
+                    break;
+            }
+            var l1 = _bus.State.Registers.L1;
+            Error($"L1 Cache, PC: 0x{l1.PC:X8}, OpCode: {l1.IID} [{l1.IID.getInstruction()}]");
+            Error($"\t0x{l1.r1:X} 0x{l1.r2:X} 0x{l1.r3:X} 0x{l1.u1:X} 0x{l1.u2:X} 0x{l1.x1:X} 0x{l1.x2:X}");
+            var l2 = _bus.State.Registers.L2;
+            Error($"L2 Cache, PC: 0x{l2.PC:X8}, OpCode: {l2.IID} [{l2.IID.getInstruction()}]");
+            Error($"\t0x{l2.r1:X} 0x{l2.r2:X} 0x{l2.r3:X} 0x{l2.u1:X} 0x{l2.u2:X} 0x{l2.x1:X} 0x{l2.x2:X}");
         }
+        private void Error(string str)
+        {
+            ForegroundColor = ConsoleColor.Red;
+            WriteLine(str);
+            ForegroundColor = ConsoleColor.White;
+        }
+
     }
 }
