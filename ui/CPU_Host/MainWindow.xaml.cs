efe8d42678aba672a6381aa9125796dcec342f7a
Yuuki Wesp <ls-micro@ya.ru>
2019-06-14 09:33:05 +0300

    upd

--- ui/CPU_Host/MainWindow.xaml.cs
+++ ui/CPU_Host/MainWindow.xaml.cs
@@ -3,13 +3,16 @@
     using System;
     using System.ComponentModel;
     using System.IO;
+    using System.Linq;
     using System.Runtime.CompilerServices;
     using System.Threading;
     using System.Threading.Tasks;
     using System.Windows;
+    using System.Windows.Controls;
     using System.Windows.Media.Imaging;
+    using System.Windows.Shapes;
+    using flame.runtime;
     using JetBrains.Annotations;
-    using MahApps.Metro.Controls;
     using vm.component;
     using vm.dev;
 
@@ -19,10 +22,11 @@
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public bool IsLoading { get; set; } = true;
-
+        public static MainWindow Singleton { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            Singleton = this;
             Icon = new BitmapImage(new Uri($"file://{new FileInfo("./resource/icon.png").FullName}"));
             DataContext = this;
             new Thread(() =>
@@ -30,18 +34,72 @@
                 Thread.Sleep(5000);
                 IsLoading = false;
                 OnPropertyChanged(nameof(IsLoading));
+                
             }).Start();
+            StartUpCPU();
+            var arr = new []{block_01, block_02, block_03, block_04, block_05, block_06, block_07, block_08}
+                .Select((x, i) => new LampBus.LampControl(i, x.Children.OfType<Ellipse>().ToArray())).ToArray();
+            Task.Factory.StartNew(async () => { await WarmUpAllDiods(arr); });
+
+        }
+
+        public async Task WarmUpAllDiods(LampBus.LampControl[] arr)
+        {
+            var block = arr.Length;
+            var col = arr.First().Diods.Length;
+            for (var u = 0; u != block; u++)
+            for (var v = 0; v != col; v++)
+            {
+                arr[u].TurnOn(v);
+                await Task.Delay(100);
+            }
+            for (var u = 0; u != block; u++)
+            for (var v = 0; v != col; v++)
+            {
+                arr[u].TurnOff(v);
+                await Task.Delay(100);
+            }
+            await Task.Factory.StartNew(async () =>
+            {
+                await Task.Delay(3000);
+                while (true)
+                {
+                    Console.Beep();
+                    await HostContainer.Instance.bus.Cpu.Step();
+                    await Task.Delay(600);
+                }
+            });
         }
         public void StartUpCPU()
         {
             var bus = HostContainer.Instance.bus;
 
-            bus.Add(new Terminal(0x1));
-            bus.Add(new AdvancedTerminal(0x2));
+            bus.State.Registers = new WPFShadowCacheFactory();
+
+            bus.Add(new LampBus(this, 
+                block_01, block_02, block_03, block_04, block_05, block_06, block_07, block_08));
 
             var core = bus.Cpu;
             
+            
+
             core.State.tc = Environment.GetEnvironmentVariable("FLAME_TRACE") == "1";
+
+            core.State.Load(new ref_t(0x6));
+            core.State.Load(new loadi(0x2,  0x11));
+            core.State.Load(new loadi(0x3,  0x1));
+
+            foreach (var i in Enumerable.Range(0, 5))
+            {
+                core.State.Load(new sum(0x4,  0x2, 0x3));
+                core.State.Load(new sum(0x2,  0x2, 0x3));
+                core.State.Load(new push_d(0xB, 0xD, 0x4));
+                core.State.Load(new push_d(0xB, 0xE, 0x4));
+            }
+
+
+
+            core.State.Load(new jump_t(0x6));
         }
 
 
@@ -66,10 +124,140 @@
         }
     }
 
+    public class WPFShadowCacheFactory : ShadowCache<Cache>
+    {
+        public Cache L1 { get; set; } = new WPFCache();
+
+        public Cache L2 { get; set; } = new Cache();
+
+        public void Reflect()
+        {
+            L2 = (Cache)L1.Clone();
+        }
+    }
+
+    public class WPFCache : Cache
+    {
+        private ushort _iid;
+        private ushort _r1;
+        private ushort _r2;
+        private ushort _r3;
+        private ushort _u1;
+        private ushort _u2;
+        private ushort _x1;
+        private ushort _x2;
+        private ulong _pc;
+
+        public override ushort IID
+        {
+            get => _iid;
+            set
+            {
+                _iid = value;
+                MainWindow.Singleton.Dispatcher.Invoke(() => { MainWindow.Singleton.IC.Content = $"0x{value:X8}"; });
+            }
+        }
+
+        public override ulong PC
+        {
+            get => _pc;
+            set
+            {
+                _pc = value;
+                MainWindow.Singleton.Dispatcher.Invoke(() => { MainWindow.Singleton.PC.Content = $"0x{value:X8}"; });
+            }
+        }
+
+        public override ushort r1
+        {
+            get => _r1;
+            set
+            {
+                _r1 = value;
+                MainWindow.Singleton.Dispatcher.Invoke(() =>
+                {
+                    MainWindow.Singleton.regBox.Items.OfType<ListBoxItem>().ToArray()[0].Content = $"0x{value:X2}";
+                });
+            }
+        }
+        public override ushort r2
+        {
+            get => _r2;
+            set
+            {
+                _r2 = value;
+                MainWindow.Singleton.Dispatcher.Invoke(() =>
+                {
+                    MainWindow.Singleton.regBox.Items.OfType<ListBoxItem>().ToArray()[1].Content = $"0x{value:X2}";
+                });
+            }
+        }
+        public override ushort r3
+        {
+            get => _r3;
+            set
+            {
+                _r3 = value;
+                MainWindow.Singleton.Dispatcher.Invoke(() =>
+                {
+                    MainWindow.Singleton.regBox.Items.OfType<ListBoxItem>().ToArray()[2].Content = $"0x{value:X2}";
+                });
+            }
+        }
+        public override ushort u1
+        {
+            get => _u1;
+            set
+            {
+                _u1 = value;
+                MainWindow.Singleton.Dispatcher.Invoke(() =>
+                {
+                    MainWindow.Singleton.regBox.Items.OfType<ListBoxItem>().ToArray()[3].Content = $"0x{value:X2}";
+                });
+            }
+        }
+        public override ushort u2
+        {
+            get => _u2;
+            set
+            {
+                _u2 = value;
+                MainWindow.Singleton.Dispatcher.Invoke(() =>
+                {
+                    MainWindow.Singleton.regBox.Items.OfType<ListBoxItem>().ToArray()[4].Content = $"0x{value:X2}";
+                });
+            }
+        }
+        public override ushort x1
+        {
+            get => _x1;
+            set
+            {
+                _x1 = value;
+                MainWindow.Singleton.Dispatcher.Invoke(() =>
+                {
+                    MainWindow.Singleton.regBox.Items.OfType<ListBoxItem>().ToArray()[5].Content = $"0x{value:X2}";
+                });
+            }
+        }
+        public override ushort x2
+        {
+            get => _x2;
+            set
+            {
+                _x2 = value;
+                MainWindow.Singleton.Dispatcher.Invoke(() =>
+                {
+                    MainWindow.Singleton.regBox.Items.OfType<ListBoxItem>().ToArray()[6].Content = $"0x{value:X2}";
+                });
+            }
+        }
+    }
+
     public class HostContainer
     {
         public static readonly HostContainer Instance = new HostContainer();
 
-        public Bus bus { get; set; }
+        public Bus bus { get; set; } = new Bus();
     }
 }
--- vm/component/State.cs
+++ vm/component/State.cs
@@ -28,22 +28,26 @@
         /// <summary>
         /// base register cell
         /// </summary>
-        public ushort r1, r2, r3;
+        public virtual ushort r1 { get; set; }
+        public virtual ushort r2 { get; set; }
+        public virtual ushort r3 { get; set; }
         /// <summary>
         /// value register cell
         /// </summary>
-        public ushort u1, u2;
+        public virtual ushort u1 { get; set; }
+        public virtual ushort u2 { get; set; }
         /// <summary>
         /// magic cell
         /// </summary>
-        public ushort x1, x2;
+        public virtual ushort x1 { get; set; }
+        public virtual ushort x2 { get; set; }
 
         /// <summary>
         /// id
         /// </summary>
-        public ushort IID;
+        public virtual ushort IID { get; set; }
 
-        public ulong PC { get; set; }
+        public virtual ulong PC { get; set; }
 
         public object Clone() => new Cache 
             { r1 = r1, r2 = r2, r3 = r3, u1 = u1, u2 = u2, x1 = x1, x2 = x2, IID = IID, PC = PC };
