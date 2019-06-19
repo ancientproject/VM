dc85e9eccc6f21e411ea8cfb5f4d668994787c1c
Yuuki Wesp <ls-micro@ya.ru>
2019-06-19 03:25:54 +0300

    add exceptions

---
+++ acc/exceptions/FlameCompileException.cs
@@ -0,0 +1,9 @@
+﻿namespace flame.compiler.exceptions
+{
+    using System;
+
+    public class FlameCompileException : Exception
+    {
+        public FlameCompileException(string msg) : base(msg){}
+    }
+}
