6dad81dc9227725cb7defa83220616799df77314
Yuuki Wesp <ls-micro@ya.ru>
2019-06-20 08:24:49 +0300

    add cpp vm

---
+++ vm/cpp/vm_cpp/abstractDevice.cpp
@@ -0,0 +1,19 @@
+#include "abstractDevice.h"
+
+void abstractDevice::write(int address, int data)
+{
+}
+
+int abstractDevice::read(int address)
+{
+    return 0;
+}
+
+void abstractDevice::init()
+{
+    
+}
+
+void abstractDevice::shutdown()
+{
+}
---
+++ vm/cpp/vm_cpp/abstractDevice.h
@@ -0,0 +1,20 @@
+#pragma once
+/**
+ * \brief declare get/set field
+ * \param type type value
+ * \param var variable name
+ */
+#define GSF(type, var)  private: type var; \
+    public: type get_##var() const { return var; } \
+            void set_##var(type val) { var = val; }
+class abstractDevice
+{
+    GSF(short, addr);
+public:
+    virtual ~abstractDevice() = default;
+    virtual void write(int address, int data);
+    virtual int read(int address);
+    virtual void init();
+    virtual void shutdown();
+};
+
