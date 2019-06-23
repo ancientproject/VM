15a95ee32a2fa9404ed5e5aaaeda484744bc4049
Yuuki Wesp <ls-micro@ya.ru>
2019-06-23 14:00:58 +0300

    add stopwatch and unicaster

---
+++ libs/Flame.Runtime/tools/StopwatchOperation.cs
@@ -0,0 +1,39 @@
+﻿namespace System.Linq
+{
+    using Diagnostics;
+
+    public class StopwatchOperation : IDisposable
+    {
+        public string Name { get; set; }
+        protected Stopwatch InnerStopwatch { get; set; }
+
+        public StopwatchOperation(string name)
+        {
+            InnerStopwatch = new Stopwatch();
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+
+            InnerStopwatch.Start();
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing) return;
+            if (InnerStopwatch == null) return;
+
+            InnerStopwatch.Stop();
+            Trace.WriteLine(
+                $"Duration of '{Name}' is {InnerStopwatch.ElapsedMilliseconds} milliseconds.");
+            InnerStopwatch = null;
+        }
+        #endregion
+    }
+}
---
+++ libs/Flame.Runtime/tools/Unicast.cs
@@ -0,0 +1,7 @@
+﻿namespace flame.runtime
+{
+    public class Unicast<TIn, TOut> where TIn : struct where TOut : struct
+    {
+        public static TIn operator &(Unicast<TIn, TOut> s, TOut q) => (TIn)(object)q;
+    }
+}
