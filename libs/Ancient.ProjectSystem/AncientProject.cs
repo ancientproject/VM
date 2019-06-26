3924cd784cab13ece6813e0a00a2213b9ab1ee86
Yuuki Wesp <ls-micro@ya.ru>
2019-06-26 19:38:04 +0300

    add run and new command

--- libs/Ancient.ProjectSystem/AncientProject.cs
+++ libs/Ancient.ProjectSystem/AncientProject.cs
@@ -1,21 +1,31 @@
 ﻿namespace Ancient.ProjectSystem
 {
+    using System.Collections.Generic;
+    using System.IO;
+    using Newtonsoft.Json;
+
     public class AncientProject
     {
+        public string name { get; set; }
         public string version { get; set; }
         /// <summary>
         /// <see cref="string"/> or <see cref="AncientAuthor"/>
         /// </summary>
         public object author { get; set; }
+
+        public Dictionary<string, string> scripts = new Dictionary<string, string>();
+
+
+        public static AncientProject Open(FileInfo file)
+        {
+            return JsonConvert.DeserializeObject<AncientProject>(File.ReadAllText(file.FullName));
+        }
     }
+
 
     public class AncientAuthor
     {
         public string name;
         public string email;
-    }
-    public class AncientElement
-    {
-
     }
 }
