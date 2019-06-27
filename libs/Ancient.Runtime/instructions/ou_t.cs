de4f833096e5f25f23235f6ac74fa68b8ef8116c
Yuuki Wesp <ls-micro@ya.ru>
2019-06-28 00:26:18 +0300

    add impl brk, ou_t

---
+++ libs/Ancient.Runtime/instructions/ou_t.cs
@@ -0,0 +1,21 @@
+﻿namespace ancient.runtime
+{
+    public class ou_t : Instruction
+    {
+        internal readonly byte _addressBus;
+        internal readonly byte _addressDev;
+        internal readonly byte _cellValue;
+
+        public ou_t(byte addressBus, byte addressDev, byte cellValue) : base(InsID.ou_t)
+        {
+            _addressBus = addressBus;
+            _addressDev = addressDev;
+            _cellValue = cellValue;
+        }
+
+        protected override void OnCompile()
+        {
+            SetRegisters(_addressBus, _addressDev, 0, _cellValue);
+        }
+    }
+}
