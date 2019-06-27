583b9743a4fe7da49763cdbd138afcb925f0ffce
Yuuki Wesp <ls-micro@ya.ru>
2019-06-28 00:27:19 +0300

    add normal bios support, hpet and etc

--- vm/csharp/component/BIOS.cs
+++ vm/csharp/component/BIOS.cs
@@ -1,83 +1,60 @@
 ﻿namespace vm.component
 {
     using System;
-    using ancient.runtime;
-    using System.Collections.Generic;
-    using System.Drawing;
-    using System.Linq;
-
-    public static class BIOS
+    using System.Diagnostics;
+    using System.Threading;
+    using dev;
+    using static State;
+    public class BIOS : AbstractDevice
     {
-        public static IReadOnlyCollection<uint> GetILCode()
-        {
-            /* @0x11 */
-            var tc = Environment.GetEnvironmentVariable("FLAME_TRACE") == "1";
-            /* @0x12 */
-            var ec = Environment.GetEnvironmentVariable("FLAME_ERROR") != "0";
-            /* @0x13 */
-            var km = Environment.GetEnvironmentVariable("FLAME_KEEP_MEMORY") == "1";
-            /* @0x14 */
-            var fw = Environment.GetEnvironmentVariable("FLAME_MEM_FAST_WRITE") == "1";
+        private readonly CPU _cpu;
+        private readonly Bus _bus;
+        public Stopwatch systemTimer;
+        private DateTime startTime;
 
-            var list = new List<uint>
+        private ulong[] mem = new ulong[32];
+
+        public bool hpet
+        {
+            set => mem[0x1] = value ? 0x1UL : 0x0UL;
+            get => mem[0x1] == 0x1;
+        }
+
+        public long Ticks 
+            => hpet ? systemTimer.ElapsedTicks : Environment.TickCount;
+
+        public BIOS(CPU cpu, Bus bus) : base(0x0, "<chipset>")
+        {
+            _cpu = cpu;
+            _bus = bus;
+        }
+
+        public override int read(int address) => (address, _bus.State.ff) switch
             {
-                /* init memory */
-                new loadi_x(0xFF, 0x20, 0xF),
-                /* set flagms */
-                new loadi_x(0x11, tc),
-                new loadi_x(0x12, ec),
-                new loadi_x(0x13, km),
-                new loadi_x(0x14, fw),
+                (0x0, _) => unchecked((int)Ticks),
+            _        => throw ThrowMemoryRead(_bus.State.curAddr, address)
             };
-            return list.AsReadOnly();
-        }
 
-
-        private const string ESC = "\x1b";
-
-        public static IReadOnlyCollection<uint> _GetILCode()
+        public override void write(int address, int data)
         {
-            var list = new List<uint>();
-
-            list.Add(new warm());
-
-            list.AddRange(WriteLine("⠜ ⠗ ⠅ ⠷ ⠷ ⠾ ⠾ ⠵ ⠸ ⠅ ⠚ ⠜ ⠵ ⠇", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("-    ------   ----  --- ------", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("00   PWR-BS   BASE  [+] [PASS]", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("01   BIOS-X   BASE  [+] [PASS]", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("02   SYS-CE   BASE  [+] [PASS]", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("-    ------   ----  --- ------", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("⠚ ⠷ ⠸ ⠵ ⠇ ⠅ ⠗ ⠾ ⠾ ⠅ ⠜ ⠜ ⠷ ⠙", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("kern.mod.cmn ....... [3 PASSED]", Color.LawnGreen).Select(x => (uint)x));
-
-            list.AddRange(WriteLine("booting up...", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("Flame BIOS // VER_1.2 :: DEFAULT", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("Mount devices...", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("⠵ ⠙ ⠚ ⠷ ⠜ ⠷ ⠗ ⠅ ⠅ ⠾ ⠵ ⠸ ⠇ ⠾ ⠜", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("⠵ ⠷ ⠜ ⠇ ⠜ ⠙ ⠷ ⠅ ⠚ ⠾ ⠸ ⠅ ⠵ ⠾ ⠗", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("-    ------   ------   --- ------", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("00   SYS-ID   LFS-CK   [+] [PASS]", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("01   SYS-MC   UFS-CK   [+] [PASS]", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("02   ABL-SS   SUB-CK   [+] [PASS]", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("03   RUN-XX   RUN-CK   [+] [PASS]", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("-    ------   ------   --- ------", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("vis_sys.mod.cmn ...... [4 PASSED]", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("⠇ ⠗ ⠙ ⠚ ⠜ ⠾ ⠅ ⠅ ⠵ ⠷ ⠸ ⠵ ⠷ ⠜ ⠾", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("⠜ ⠷ ⠷ ⠾ ⠚ ⠜ ⠾ ⠸ ⠵ ⠅ ⠙ ⠅ ⠇ ⠵ ⠗", Color.LawnGreen).Select(x => (uint)x));
-            list.AddRange(WriteLine("Execute main code...", Color.LawnGreen).Select(x => (uint)x));
-
-            return list.AsReadOnly();
+            _ = address switch {
+                0x1 => X(() => hpet = data == 0x1),
+                0xF => X(Init),
+                0xD => X(() => Thread.Sleep(data)),
+                _   => X(() => ThrowMemoryWrite(_bus.State.curAddr, address))
+            };
         }
 
-        private static Instruction[] WriteLine(string line, Color color)
+        public int X(Action _)
         {
-            line = $"{line}{Environment.NewLine}";
-            var result = $"{GetColorForegroundString(color)}{line}{GetColorForegroundString(Color.White)}";
-
-            return result.Select(x => new push_a(0x2, 0x5, x)).Cast<Instruction>().ToArray();
+            _();
+            return 0;
         }
 
-        private static string GetColorForegroundString(Color c) => 
-            string.Concat(ESC, "[38;2;", c.R.ToString(), ";", c.G.ToString(), ";", c.B.ToString(), "m");
+        public override void Init()
+        {
+            systemTimer = Stopwatch.StartNew();
+            startTime = DateTime.UtcNow;
+        }
     }
 }
