2549b9a7e865a629165a17928a47813013c08c3b
Yuuki Wesp <ls-micro@ya.ru>
2019-06-28 00:28:16 +0300

    add support env variables to externalTools runner

--- Rune/cli/ExternalTools.cs
+++ Rune/cli/ExternalTools.cs
@@ -1,5 +1,6 @@
 ﻿namespace rune.cli
 {
+    using System.Collections.Generic;
     using System.Diagnostics;
 
     public class ExternalTools
@@ -7,7 +8,7 @@
         private readonly string _cmd;
         private readonly string _args;
         private readonly Process proc;
-        public ExternalTools(string cmd, string args)
+        public ExternalTools(string cmd, string args, IDictionary<string, string> env = null)
         {
             _cmd = cmd;
             _args = args;
@@ -15,8 +16,19 @@
             {
                 StartInfo = new ProcessStartInfo(cmd, args)
                 {
+                    
                 }
             };
+            if (env is null) 
+                return;
+            foreach (var (key, value) in env) 
+                proc.StartInfo.Environment.Add(key, value);
+        }
+
+        public ExternalTools WithEnv(string flagName, bool value)
+        {
+            proc.StartInfo.Environment.Add(flagName, value ? "1" : "0");
+            return this;
         }
 
         public ExternalTools Start()
