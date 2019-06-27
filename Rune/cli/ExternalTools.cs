56416054eae1c68e1b66ecdca9bcb524c7fb5ad9
Yuuki Wesp <ls-micro@ya.ru>
2019-06-27 18:16:52 +0300

    add external tools executer

---
+++ Rune/cli/ExternalTools.cs
@@ -0,0 +1,36 @@
+﻿namespace rune.cli
+{
+    using System.Diagnostics;
+
+    public class ExternalTools
+    {
+        private readonly string _cmd;
+        private readonly string _args;
+        private readonly Process proc;
+        public ExternalTools(string cmd, string args)
+        {
+            _cmd = cmd;
+            _args = args;
+            proc = new Process
+            {
+                StartInfo = new ProcessStartInfo(cmd, args)
+                {
+                }
+            };
+        }
+
+        public ExternalTools Start()
+        {
+            proc.Start();
+            return this;
+        }
+
+        public ExternalTools Wait()
+        {
+            proc.WaitForExit();
+            return this;
+        }
+
+        public int ExitCode() => proc.ExitCode;
+    }
+}
