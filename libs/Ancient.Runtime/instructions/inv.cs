0097a354b17d37b6ba35fc90642aa96c96d742cd
Yuuki Wesp <ls-micro@ya.ru>
2019-06-28 04:06:38 +0300

    add sig, inv, ret

---
+++ libs/Ancient.Runtime/instructions/inv.cs
@@ -0,0 +1,24 @@
+﻿namespace ancient.runtime
+{
+    public class inv : Instruction
+    {
+        private readonly byte _sigId;
+
+        public inv(byte sigID) : base(InsID.inv) => _sigId = sigID;
+        protected override void OnCompile() => SetRegisters(r2: _sigId);
+    }
+
+    public class sig : Instruction
+    {
+        private readonly byte _sigId;
+
+        public sig(byte sigID) : base(InsID.sig) => _sigId = sigID;
+        protected override void OnCompile() => SetRegisters(r2: _sigId);
+    }
+
+    public class ret : Instruction
+    {
+        public ret() : base(InsID.ret) { }
+        protected override void OnCompile() { }
+    }
+}
