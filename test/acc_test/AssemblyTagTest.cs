1ea24b69824b0e650284602e556f3fea2cfca985
Yuuki Wesp <ls-micro@ya.ru>
2019-07-02 03:41:58 +0300

    fix assemblyTag test

--- test/acc_test/AssemblyTagTest.cs
+++ test/acc_test/AssemblyTagTest.cs
@@ -9,8 +9,8 @@
         public void Create()
         {
             Assert.True(AssemblyTag.IsTag("EF0109JG00"));
-            Assert.Equal("EF0119JG00", new AssemblyTag(AssemblyTag.SignType.Signed, AssemblyTag.ArchType.Any, 1).ToString());
-            Assert.Equal("EF0500JG00", new AssemblyTag(AssemblyTag.SignType.UnSecurity, AssemblyTag.ArchType.x64, 5).ToString());
+            Assert.Contains("EF0119J", new AssemblyTag(AssemblyTag.SignType.Signed, AssemblyTag.ArchType.Any, 1).ToString());
+            Assert.Contains("EF0500J", new AssemblyTag(AssemblyTag.SignType.UnSecurity, AssemblyTag.ArchType.x64, 5).ToString());
         }
     }
 }
