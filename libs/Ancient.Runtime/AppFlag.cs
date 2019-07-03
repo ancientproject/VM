b08e23201cb8ba26c3c7821360d47648977a4449
Yuuki Wesp <ls-micro@ya.ru>
2019-07-03 04:20:41 +0300

    add AppFlag

---
+++ libs/Ancient.Runtime/AppFlag.cs
@@ -0,0 +1,24 @@
+﻿namespace ancient.runtime
+{
+    using System;
+
+    public static class AppFlag
+    {
+        public static bool GetVariable(string code, bool? @default = null)
+        {
+            if (code == null) throw new ArgumentNullException(nameof(code));
+
+            var result = Environment.GetEnvironmentVariable(code)?.ToLowerInvariant();
+
+            if (result is null && @default is null)
+                return false;
+            if (result is null)
+                return @default.Value;
+            return    result == "1" 
+                   || result == "true" 
+                   || result == "on" 
+                   || result == "enable" 
+                   || result == "yes";
+        }
+    }
+}
