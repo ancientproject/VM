0cb96cdeb93c0d0c6bf2cd29a61342fd8cade7c6
Yuuki Wesp <ls-micro@ya.ru>
2019-07-03 04:52:16 +0300

    add ancient.runtime.loader

---
+++ libs/Ancient.Runtime.Context/DeviceLoadContext.cs
@@ -0,0 +1,32 @@
+﻿namespace ancient.runtime.context
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.Loader;
+
+    public class DeviceLoadContext : AssemblyLoadContext
+    {
+        public DeviceLoadContext() : base("dev-loader", true) { }
+
+        protected override Assembly Load(AssemblyName assemblyName)
+        {
+            var devName = $"{assemblyName.FullName}|-managed.dll";
+            var file = new[]
+            {
+                new FileInfo($"./dev/{devName}"),
+                new FileInfo($"./../dev/{devName}"),
+                new FileInfo($"./../../dev/{devName}"),
+                new FileInfo($"{Environment.GetEnvironmentVariable("VM_DEV_HOME")}/{devName}")
+            }.FirstOrDefault(x => x.Exists);
+
+            if (file is null)
+                return null;
+            return Assembly.LoadFile(file.FullName);
+        }
+
+        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName) 
+            => throw new Exception($"can't load '{unmanagedDllName}|0.0.0-unmanaged.dll' - loader is not supported.");
+    }
+}
