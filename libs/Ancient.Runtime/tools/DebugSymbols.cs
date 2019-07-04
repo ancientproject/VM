1b500b78186b4b53b1da46ead2594c1e9dce4570
Yuuki Wesp <ls-micro@ya.ru>
2019-07-04 09:21:27 +0300

    add getting encoding debug symbols from env

--- libs/Ancient.Runtime/tools/DebugSymbols.cs
+++ libs/Ancient.Runtime/tools/DebugSymbols.cs
@@ -8,18 +8,18 @@
     public class DebugSymbols
     {
         public List<(short offset, string line)> symbols = new List<(short offset, string line)>();
-
+        public static string SymbolEncoding => Environment.GetEnvironmentVariable("VM_SYM_ENCODING") ?? "IBM037";
 
         public static DebugSymbols Open(byte[] file)
         {
-            var str = Convert.FromBase64String(Encoding.GetEncoding("IBM037").GetString(file));
+            var str = Convert.FromBase64String(Encoding.GetEncoding(SymbolEncoding).GetString(file));
             return JsonConvert.DeserializeObject<DebugSymbols>(Encoding.UTF8.GetString(str));
         }
 
         public static byte[] ToBytes(DebugSymbols sym)
         {
             var bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(sym));
-            return Encoding.GetEncoding("IBM037").GetBytes(Convert.ToBase64String(bytes));
+            return Encoding.GetEncoding(SymbolEncoding).GetBytes(Convert.ToBase64String(bytes));
         }
     }
 }
