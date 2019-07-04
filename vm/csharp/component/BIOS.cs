4fd6536d09d05de30c0141caecf900d4827658ad
Yuuki Wesp <ls-micro@ya.ru>
2019-07-04 23:35:31 +0300

    improvement bios

--- vm/csharp/component/BIOS.cs
+++ vm/csharp/component/BIOS.cs
@@ -2,9 +2,11 @@
 {
     using System;
     using System.Diagnostics;
+    using System.Drawing;
     using System.Threading;
     using ancient.runtime;
-    using dev;
+    using Pastel;
+
     public class BIOS : Device
     {
         private readonly CPU _cpu;
@@ -23,16 +25,17 @@
         public long Ticks 
             => hpet ? systemTimer.ElapsedTicks : Environment.TickCount;
 
-        public BIOS(CPU cpu, Bus bus) : base(0x0, "<chipset>")
+        public BIOS(CPU cpu, Bus bus) : base(0x45, "<chipset>")
         {
             _cpu = cpu;
             _bus = bus;
             this.hpet = AppFlag.GetVariable("c69_bios_hpet");
         }
 
-        public override long read(long address) => (address, _bus.State.ff) switch
-            {
-                (0x0, _) => unchecked((int)Ticks),
+        public override long read(long address) => (address, _bus.State.ff) switch {
+                    (0x0, _) => unchecked((int) Ticks),
+                    (0x1, _) => hpet ? 0x1 : 0x0,
+                    (0x2, _) => _bus.State.memoryChannel,
             _        => throw ThrowMemoryRead(_bus.State.curAddr, address)
             };
 
@@ -44,6 +47,15 @@
                 0xD => X(() => Thread.Sleep((int)data)),
                 _   => X(() => ThrowMemoryWrite(_bus.State.curAddr, address))
             };
+        }
+
+        public override void shutdown()
+        {
+            systemTimer.Stop();
+            var line = $"    system is stopped, total operating time: {systemTimer.Elapsed:g}    ";
+            Console.WriteLine(new string(' ', line.Length).PastelBg(Color.DarkRed));
+            Console.WriteLine(line.PastelBg(Color.DarkRed));
+            Console.WriteLine(new string(' ', line.Length).PastelBg(Color.DarkRed));
         }
 
         public int X(Action _)
