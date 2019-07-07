09677c33312d569bd59efaa31555773cdca05189
Yuuki Wesp <ls-micro@ya.ru>
2019-07-07 12:26:30 +0300

    add auto generate lock file from assembly

--- libs/Ancient.ProjectSystem/AncientLockFile.cs
+++ libs/Ancient.ProjectSystem/AncientLockFile.cs
@@ -1,9 +1,23 @@
 ﻿namespace Ancient.ProjectSystem
 {
     using System;
+    using System.IO;
+    using System.Reflection;
+    using Newtonsoft.Json;
 
     public class AncientLockFile
     {
+        [JsonProperty(Order = 0)]
+        public string IS_AUTO_GENERATED { get; set; } = "DON'T TOUCH THIS FILE";
+
+        public AncientLockFile() {}
+        public AncientLockFile(Assembly asm, string Registry)
+        {
+            id = Path.GetFileNameWithoutExtension(asm.GetName().Name);
+            registry = Registry;
+            version = asm.GetName().Version;
+        }
+
         public string id { get; set; }
         public string registry { get; set; }
         public Version version { get; set; }
