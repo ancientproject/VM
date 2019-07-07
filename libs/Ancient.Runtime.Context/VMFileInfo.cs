b098cc68225750542cb257418e74450a1943cd86
Yuuki Wesp <ls-micro@ya.ru>
2019-07-07 06:27:12 +0300

    add VMFileInfo

---
+++ libs/Ancient.Runtime.Context/VMFileInfo.cs
@@ -0,0 +1,26 @@
+﻿namespace ancient.runtime.context
+{
+    using System.IO;
+
+    public class VMFileInfo : FileSystemInfo
+    {
+        public VMFileInfo(string fileName)
+        {
+            Name = fileName;
+            try
+            {
+                raw = new FileInfo(fileName);
+                Exists = raw.Exists;
+            }
+            catch { }
+        }
+
+        private FileInfo raw { get; }
+
+        public override void Delete() => raw?.Delete();
+
+        public override bool Exists { get; }
+        public override string Name { get; }
+        public override string ToString() => Name;
+    }
+}
