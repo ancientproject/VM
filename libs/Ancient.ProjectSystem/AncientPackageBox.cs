3be9fc5c9c9cdc4bdad6de74568d3d0abb26be8d
Yuuki Wesp <ls-micro@ya.ru>
2019-07-07 12:26:39 +0300

    add package box

---
+++ libs/Ancient.ProjectSystem/AncientPackageBox.cs
@@ -0,0 +1,10 @@
+﻿namespace Ancient.ProjectSystem
+{
+    using System.Collections.Generic;
+
+    public class AncientPackageBox
+    {
+        public string type { get; set; }
+        public List<string> files { get; set; }
+    }
+}
