13aa9108c9b66ac75a2352953f7c70f5e6d497fa
Yuuki Wesp <ls-micro@ya.ru>
2019-07-07 06:29:27 +0300

    improvement deviceLoad

--- libs/Ancient.Runtime.Context/DeviceImageLoadContext.cs
+++ libs/Ancient.Runtime.Context/DeviceImageLoadContext.cs
@@ -1,44 +1,53 @@
 ﻿namespace ancient.runtime.context
 {
+    using MoreLinq;
     using System;
     using System.Diagnostics;
     using System.IO;
     using System.Linq;
     using System.Reflection;
-    using System.Runtime.InteropServices;
     using System.Runtime.Loader;
 
     public class DeviceImageLoadContext : AssemblyLoadContext
     {
         private readonly Action<string> _trace;
-        public DeviceImageLoadContext(Action<string> trace = null) : base("dev-loader", true) => _trace = trace ?? (x => {});
+        public DeviceImageLoadContext(Action<string> trace = null) 
+            : base("dev-loader", true) 
+            => _trace = trace ?? (x => {});
 
         public DirectoryInfo CacheDir
         {
             get
             {
-                var dir = Path.Combine(
-                    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), ".ancient/cache/dev");
-                return Directory.CreateDirectory(dir);
+                var dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".ancient");
+                return Directory.CreateDirectory(dir).CreateSubdirectory("packages");
             }
         }
 
+        private void log(string s) => _trace($"[image-load-context] {s}");
+
+        public FileSystemInfo FindImage(string devName, Version version)
+        {
+            var corePath = $"deps/{version.Major}.{version.Minor}.{version.Build}/any/{devName}.image";
+            return new FileSystemInfo[]
+            {
+                new FileInfo($"./{corePath}"),
+                new FileInfo($"./../{corePath}"),
+                new FileInfo($"./../../{corePath}"),
+                new FileInfo($"{Environment.GetEnvironmentVariable("VM_DEV_HOME") ?? "vm:/home"}/{corePath}"),
+                new VMFileInfo(Path.Combine(CacheDir.FullName, corePath).Replace("\\", "/"))
+            }.Pipe(x => log($"try find '{devName}' in '{x}'")).FirstOrDefault(x => x.Exists);
+        }
 
         protected override Assembly Load(AssemblyName assemblyName)
         {
-            var devName = $"{assemblyName.Name}.image";
-            var file = new[] 
-            {
-                new FileInfo($"./dev/{devName}"),
-                new FileInfo($"./../dev/{devName}"),
-                new FileInfo($"./../../dev/{devName}"),
-                new FileInfo($"{Environment.GetEnvironmentVariable("VM_DEV_HOME")}/{devName}"),
-                new FileInfo(Path.Combine(CacheDir.FullName, $"{devName}"))
-            }.FirstOrDefault(x => x.Exists);
-            
+            var imageName = assemblyName.Name;
+            var fullImageName = $"{assemblyName.Name}.image";
+            var imageVersion = assemblyName.Version;
+            var file = FindImage(imageName, imageVersion);
             if (file is null)
-                throw new Exception($"can't load '{assemblyName.Name}|0.0.0-managed.image' - [not found].");
-            (_trace + (q => Trace.WriteLine(q)))($"'{devName}' can find in '{file}' and load...");
+                throw new Exception($"can't load '{fullImageName}' - [not found].");
+            log($"'{fullImageName}' was found in '{file}' and success loaded");
             return Assembly.LoadFile(file.FullName);
         }
 
