c55a8a57e146ed096a27e79267782eccb7b18477
Yuuki Wesp <ls-micro@ya.ru>
2019-07-07 06:27:46 +0300

    add rune test

---
+++ Rune/etc/CSharpCompile.cs
@@ -0,0 +1,47 @@
+﻿namespace rune.etc
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.Loader;
+    using System.Text;
+    using ancient.runtime;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+
+    public class CSharpCompile
+    {
+        public static Assembly Build(string id, string code)
+        {
+            var dd = typeof(Enumerable).GetTypeInfo().Assembly.Location;
+            var coreDir = Directory.GetParent(dd);
+
+            var refs = coreDir.GetFiles("*.dll").Select(x => MetadataReference.CreateFromFile(x.FullName)).ToList();
+
+            refs.AddRange(new []
+            {
+                MetadataReference.CreateFromFile(typeof(ldx).GetTypeInfo().Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(IDevice).GetTypeInfo().Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Console).GetTypeInfo().Assembly.Location)
+            });
+
+            var compilation = CSharpCompilation.Create($"{id}.image")
+                .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, allowUnsafe: true))
+                .AddReferences(refs)
+                .AddSyntaxTrees(CSharpSyntaxTree.ParseText(code));
+
+            using var ms = new MemoryStream();		
+            var result = compilation.Emit(ms);
+            if (!result.Success)
+            {
+                var b = new StringBuilder();
+                foreach (var diagnostic in result.Diagnostics)
+                    b.AppendLine(diagnostic.ToString());
+                throw new Exception(b.ToString());
+            }
+            ms.Seek(0, SeekOrigin.Begin);
+            return AssemblyLoadContext.Default.LoadFromStream(ms);
+        }
+    }
+}
---
+++ test/RuneTest/UnitTest1.cs
@@ -0,0 +1,79 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    using rune.etc;
+
+    public class CSharpCompileTest
+    {
+        [Test]
+        public void Compile()
+        {
+            var result = CSharpCompile.Build("term", @"
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using ancient.runtime;
+using static System.Console;
+
+[Guid(""2F380069-1951-41DF-9500-3FE4610FA924"")]
+
+
+        public class TerminalDevice : Device
+        {
+            private StringBuilder relMemory = new StringBuilder();
+
+            public override void shutdown() => relMemory = null;
+            public TerminalDevice() : base(0x1, ""term"") { }
+
+            public override void write(long address, long data)
+            {
+                switch (address)
+                {
+                    case 0x5:
+                        var c1 = (char)data;
+                        if(c1 == 0xA)
+                            Write(Environment.NewLine);
+                        else 
+                            Write(c1);
+                        break;
+                    case 0x6:
+                        var c2 = (char)data;
+                        if (c2 == 0xA)
+                            relMemory.Append(Environment.NewLine);
+                        else 
+                            relMemory.Append(c2);
+                        break;
+                    case 0x7:
+                        Out.Write(relMemory);
+                        break;
+                    case 0x8:
+                        relMemory.Clear();
+                        break;
+                    case 0x9:
+                        var u1 = ((short)data & 0xF0) >> 4;
+                        var u2 = (short)data & 0xF;
+
+                        switch (u1)
+                        {
+                            case 0x1:
+                                ForegroundColor = (ConsoleColor)u2;
+                                break;
+                            case 0x2:
+                                BackgroundColor = (ConsoleColor)u2;
+                                break;
+                            case 0x3:
+                                ForegroundColor = ConsoleColor.White;
+                                break;
+                            case 0x4:
+                                BackgroundColor = ConsoleColor.Black;
+                                break;
+                        }
+                        break;
+                }
+            }
+        }
+");
+        }
+    }
+}
