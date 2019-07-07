0113b92fc4601413772b67b67936a01be8f0d80e
Yuuki Wesp <ls-micro@ya.ru>
2019-07-07 06:26:38 +0300

    add lock file

---
+++ libs/Ancient.ProjectSystem/AncientLockFile.cs
@@ -0,0 +1,12 @@
+﻿namespace Ancient.ProjectSystem
+{
+    using System;
+
+    public class AncientLockFile
+    {
+        public string id { get; set; }
+        public string registry { get; set; }
+        public Version version { get; set; }
+        public string platform { get; set; } = "any";
+    }
+}
