06ec3c37a8c24e697462a3b808eb1ac905b65e02
Yuuki Wesp <ls-micro@ya.ru>
2019-07-07 04:18:05 +0300

    add trace event to loader

--- libs/Ancient.Runtime.Context/DeviceLoader.cs
+++ libs/Ancient.Runtime.Context/DeviceLoader.cs
@@ -8,10 +8,12 @@
     {
         public static DeviceImageLoadContext Context { get; internal set; }
         public static event Action<string> OnTrace;
-        public static void Grub(Action<IDevice> hook, params string[] additionalImage)
+
+        public static void Grub(Action<IDevice> hook, params string[] additionalImage) => Grub(hook, null, additionalImage);
+        public static void Grub(Action<IDevice> hook, Action<string> trace, params string[] additionalImage)
         {
             if(Context is null)
-                Context = new DeviceImageLoadContext();
+                Context = new DeviceImageLoadContext(trace);
 
             var asmList = additionalImage
                 .Select(imageName => Context.LoadFromAssemblyName(new AssemblyName(imageName)))
