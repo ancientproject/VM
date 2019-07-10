cbdae9a7846e22f9b6668d73de471886227b14cd
Yuuki Wesp <ls-micro@ya.ru>
2019-07-10 06:53:57 +0300

    add chain operator

---
+++ test/RuneTest/ChainOperator.cs
@@ -0,0 +1,33 @@
+﻿namespace Tests
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal sealed class ChainOperator<T> : ChainOperator
+    {
+        public ChainOperator(ChainOperator outerChain, T value)
+        {
+            this.data = outerChain.data;
+            this.index = outerChain.index + 1;
+            this.data.Add(value);
+        }
+
+        public ChainOperator(T value)
+        {
+            this.index = 0;
+            this.data = new List<object> { value };
+        }
+
+        internal ChainOperator<T> Pipe(Action<T> act)
+        {
+            act(data[index].As<T>());
+            return this;
+        }
+    }
+
+    internal abstract class ChainOperator
+    {
+        protected internal IList<object> data { get; set; }
+        protected internal int index { get; set; }
+    }
+}
---
+++ test/RuneTest/ChainOperatorEx.cs
@@ -0,0 +1,21 @@
+﻿namespace Tests
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal static class ChainOperatorEx
+    {
+        public static N As<N>(this object s) 
+            => (N) s;
+        public static T Return<T>(this ChainOperator<T> chain) 
+            => chain.data[chain.index].As<T>();
+        public static R Return<T, R>(this ChainOperator<T> chain, Func<T, R> actor) 
+            => actor(chain.data[chain.index].As<T>());
+        public static ChainOperator<T> Chain<T, S>(this List<S> o, T t, Action<List<S>, T> actor) 
+            => new ChainOperator<T>(t).Pipe(x => actor(o, t));
+        public static ChainOperator<T> And<T>(this ChainOperator<T> chain, Action<T> actor) 
+            => chain.Pipe(actor);
+        public static ChainOperator<N> Mutate<T, N>(this ChainOperator<T> chain, Func<T, N> actor) 
+            => new ChainOperator<N>(chain, actor(chain.Return()));
+    }
+}
