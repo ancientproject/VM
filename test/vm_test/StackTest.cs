e06f61151989b9501061c062a7edf248be9eb211
Yuuki Wesp <ls-micro@ya.ru>
2019-07-10 06:57:58 +0300

    add stack test

---
+++ test/vm_test/StackTest.cs
@@ -0,0 +1,52 @@
+﻿namespace vm_test
+{
+    using System.Linq;
+    using NUnit.Framework;
+    using vm.component;
+
+    public class StackTest : VMBehaviour
+    {
+        private Stack stack;
+
+        [SetUp]
+        public void setUp()
+        {
+            stack = new Stack(bus, this, state);
+        }
+
+        [Test]
+        public void UseVirtualForward()
+        {
+            // use virtual stack
+            state.southFlag = true;
+            bios.virtual_stack = true;
+            bios.memory_stack_forward = false;
+            stack.push(0x1234567890ABCDEF);
+            Assert.AreEqual(1, state.SP);
+            Assert.AreEqual(0x1234567890ABCDEF, stack.pop());
+           
+        }
+        [Test]
+        public void UseMemStackForward()
+        {
+            state.southFlag = true;
+            bios.virtual_stack = false;
+            bios.memory_stack_forward = true;
+            stack.push(0x1234567890ABCDEF);
+            Assert.AreEqual(1, state.SP);
+            Assert.AreEqual(0x1234567890ABCDEF, stack.pop());
+        }
+        [Test]
+        public void WithoutVirtualForward()
+        {
+            // using memory stack forward
+            state.southFlag = true;
+            bios.virtual_stack = false;
+            bios.memory_stack_forward = false;
+            stack.push(0x1234567890ABCDEF);
+            Assert.AreEqual(8, state.SP);
+            Assert.AreEqual(0x1234567890ABCDEF, stack.pop());
+        }
+
+    }
+}
