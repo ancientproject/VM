cdee35aa399fb6fb6b882ed379fdebbb4680cb28
Yuuki Wesp <ls-micro@ya.ru>
2019-07-13 17:42:57 +0300

    set [Serializable]

--- acc/exceptions/AncientEvolveException.cs
+++ acc/exceptions/AncientEvolveException.cs
@@ -1,7 +1,7 @@
 ﻿namespace ancient.compiler.exceptions
 {
     using System;
-
+    [Serializable]
     public class AncientEvolveException : Exception
     {
         public AncientEvolveException(string msg) : base(msg){}
--- libs/Ancient.Runtime/exceptions/InvalidCharsException.cs
+++ libs/Ancient.Runtime/exceptions/InvalidCharsException.cs
@@ -1,7 +1,7 @@
 ﻿namespace ancient.runtime.exceptions
 {
     using System;
-
+    [Serializable]
     public class InvalidCharsException : Exception
     {
         public InvalidCharsException(string msg) : base(msg) { }
