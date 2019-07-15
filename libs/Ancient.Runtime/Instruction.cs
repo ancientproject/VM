58c506b40c2b9b7b7e1d5962712a8a14eaef0da9
Yuuki Wesp <ls-micro@ya.ru>
2019-07-15 18:05:09 +0300

    add support metadata

--- libs/Ancient.Runtime/Instruction.cs
+++ libs/Ancient.Runtime/Instruction.cs
@@ -1,15 +1,15 @@
 ﻿namespace ancient.runtime
 {
     using System;
-    using System.Diagnostics;
     using System.Linq;
     using System.Reflection;
 
-    [DebuggerDisplay("{ToString()}")]
     public abstract class Instruction : OpCode
     {
         public IID ID { get; protected set; }
         public short OPCode { get; protected set; }
+
+        private byte _r1, _r2, _r3, _u1, _u2, _x1, _x2;
 
         protected Instruction() { }
         protected Instruction(IID id)
@@ -46,35 +46,34 @@
             #pragma warning restore CS0675
         }
 
+        public override byte[] GetBodyILBytes() => BitConverter.GetBytes(Assembly());
+        public override byte[] GetMetaDataILBytes()
+        {
+            if (HasMetadata())
+                return Array.Empty<byte>();
+            return metadataBytes;
+        }
 
+        protected internal virtual byte[] metadataBytes { get; } = Array.Empty<byte>();
+        protected abstract void OnCompile();
+
+        #region serviced
 
         public static implicit operator uint(Instruction i) => (uint)i.Assembly();
         public static implicit operator ulong(Instruction i) => (ulong)i.Assembly();
 
-
-        public override byte[] GetBodyILBytes() => BitConverter.GetBytes(Assembly());
         public override string ToString() => $"{ID} [{string.Join(" ", GetBodyILBytes().Select(x => x.ToString("X2")))}]";
 
         public void Construct(byte r1 = 0, byte r2 = 0, byte r3 = 0, byte u1 = 0, byte u2 = 0, byte x1 = 0,
             byte x2 = 0) => SetRegisters(r1, r2, r3, u1, u2, x1, x2);
-        [Obsolete]
-        public void SetRegisters(byte r1 = 0, byte r2 = 0, byte r3 = 0, byte u1 = 0, byte u2 = 0, byte x1 = 0,
-            byte x2 = 0)
+        [Obsolete("use Construct")]
+        public void SetRegisters(byte r1 = 0, byte r2 = 0, byte r3 = 0, byte u1 = 0, byte u2 = 0, byte x1 = 0, byte x2 = 0)
         {
-            _r1 = r1;
-            _r2 = r2;
-            _r3 = r3;
-            _u1 = u1;
-            _u2 = u2;
-            _x1 = x1;
+            _r1 = r1; _r2 = r2;
+            _r3 = r3; _u1 = u1;
+            _u2 = u2; _x1 = x1;
             _x2 = x2;
         }
-
-        private byte _r1, _r2, _r3, _u1, _u2, _x1, _x2;
-
-        protected abstract void OnCompile();
-
-
         public static Instruction Summon(IID id, params object[] args)
         {
             var currentAsm = typeof(Instruction).Assembly;
@@ -108,5 +107,7 @@
             }
             throw new InvalidOperationException($"Not found class for '{id}' operation.");
         }
+
+        #endregion
     }
 } 
