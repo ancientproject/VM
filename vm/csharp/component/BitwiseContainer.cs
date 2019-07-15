5e54318766da00b16d8798ddab2977f1eba32a09
Yuuki Wesp <ls-micro@ya.ru>
2019-07-15 18:04:20 +0300

    set AggressiveInlining bitwise container

--- vm/csharp/component/BitwiseContainer.cs
+++ vm/csharp/component/BitwiseContainer.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections;
     using System.Linq;
+    using System.Runtime.CompilerServices;
 
     public class BitwiseContainer : IFormattable
     {
@@ -13,12 +14,15 @@
         public static implicit operator BitwiseContainer(ulong value) => new BitwiseContainer(value);
         public static implicit operator BitwiseContainer(long value) => new BitwiseContainer((ulong)value);
         public static implicit operator ulong(BitwiseContainer value) => value._value;
+
         
         public static ulong operator &(BitwiseContainer _, ulong mask)
         {
             var shift = new BitArray(BitConverter.GetBytes(mask)).Cast<bool>().TakeWhile(bit => !bit).Count();
-            return (_._value & mask) >> shift;
+            return _sh(_._value, mask, shift);
         }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static ulong _sh(ulong value, ulong mask, int bit) => (value & mask) >> bit;
 
         public override string ToString() => _value.ToString();
         public string ToString(string format, IFormatProvider formatProvider) => _value.ToString(format, formatProvider);
