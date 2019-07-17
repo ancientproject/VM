c4f03e4fc102a2c93c40b3f76e31b7d2e4152167
Yuuki Wesp <ls-micro@ya.ru>
2019-07-18 00:49:05 +0300

    update deconstruct-helper classes

--- libs/Ancient.Runtime/emit/template/UnsafeDeconstruct.cs
+++ libs/Ancient.Runtime/emit/template/UnsafeDeconstruct.cs
@@ -4,13 +4,15 @@
 
     public unsafe class UnsafeDeconstruct<TValue> where TValue : unmanaged
     {
-        protected internal TValue _value { get; }
+        protected internal TValue _value { get; set; }
         protected internal Func<int> shift => shifter.Shift;
 
         protected internal IShifter shifter { get; private set; }
         protected internal int unmanaged_size => sizeof(TValue);
         protected internal int full_size => unmanaged_size * 2 * 4;
         protected internal int size => full_size - 4;
+
+        public TValue Value => _value;
 
         protected internal void resetShifter() => shifter = ShiftFactory.Create(size);
 
--- libs/Ancient.Runtime/emit/template/d16u.cs
+++ libs/Ancient.Runtime/emit/template/d16u.cs
@@ -10,6 +10,24 @@
             n2 = (byte)((_value & 0x0F00) >> shift());
             n3 = (byte)((_value & 0x00F0) >> shift());
             n4 = (byte)((_value & 0x000F) >> shift());
+            resetShifter();
         }
+        public (byte n1, byte n2, byte n3, byte n4) Deconstruct()
+        {
+            var (n1, n2, n3, n4) = this;
+            return (n1, n2, n3, n4);
+        }
+        public d16u Construct(in byte n1, in byte n2, in byte n3, in byte n4)
+        {
+            resetShifter();
+            this._value = (ushort) ((n1 << shift()) | (n2 << shift()) | (n3 << shift()) | (n4 << shift()));
+            return this;
+        }
+
+        public static implicit operator (byte n1, byte n2, byte n3, byte n4)(d16u u) => u.Deconstruct();
+        public static implicit operator d16u((byte n1, byte n2, byte n3, byte n4) u) => new d16u(0).Construct(u.n1, u.n2, u.n3, u.n4);
+
+        public static implicit operator ushort(d16u u) => u.Value;
+
     }
 }
