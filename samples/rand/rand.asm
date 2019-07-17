0d8cbac51314d5b9294be2cd415a971d1fed7615
Yuuki Wesp <ls-micro@ya.ru>
2019-07-18 00:44:05 +0300

    upd

--- samples/rand/rand.asm
+++ samples/rand/rand.asm
@@ -2,25 +2,36 @@
     ~label 'z'    0x05 ~label 'a'    0x06 ~label 'b' 0x07
     ~label 'bios' 0x45 ~label 'tick' 0x05
 }
-{| this is fucking comment line |}
-.page ![0x600]
-.prune (0x0E1)<->(0x0D2)
-.warm
-.unlock &(0x000)
-.rf.d   &(![~bios]) &(![~tick])
-.pull   &(![~z])
-.ld.x   &(0x009)    $(0x4CE3)
-.mul    $(0x01F)    &(![~z])
-.ixor   &(0x009)    &(![~z])
-.dup    &(![~z]) |> &(0x012)
-.stl.c  &(0x012)
-.call.i !{sys->external->module(u32)}
-.add    $(0xD00) &(![~z])
-.mull   $(0xAD0) &(![~z])
-.lp.str &[{"random value: "}]
-.pull   &(0x008)
-.mv.x   &(0x015) <| &(0x008)
-.mv.x   &(0x015) <| &(![~z])
-.halt
+.dup &(![~z]) &(0x1)
 
-@float_t(12.2)
+.locals init #(
+    [0x0] u32
+    [0x1] u8
+) 
+; ...
+.prune
+.dup &(![~z]) &(0x1)
+
+.locals init #(
+    [0x0] f64,
+    [0x1] u8,
+    [0x2] u8,
+    [0x3] u8,
+    [0x4] u8,
+    [0x5] u8
+) 
+; ...
+.prune
+.dup &(![~z]) &(0x1)
+
+.locals init #(
+    [0x0] f64
+    [0x1] u8
+    [0x2] u8
+    [0x3] u8
+    [0x4] u8
+    [0x5] u8
+) 
+; ...
+.prune
+.dup &(![~z]) &(0x1)
