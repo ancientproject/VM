8a264f6e459e53c58ddc61908928d2d4a8c25fe8
Yuuki Wesp <ls-micro@ya.ru>
2019-07-19 02:05:31 +0300

    add metadata template and lpstr fixes

---
+++ libs/Ancient.Runtime/emit/MetaTemplate.cs
@@ -0,0 +1,45 @@
+﻿namespace ancient.runtime.emit.@unsafe
+{
+    using System.Runtime.InteropServices;
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct MetaTemplate
+    {
+        public int len { get; set; }
+        public TemplateType type { get; set; }
+
+        public static MetaTemplate By(TemplateType type) => new MetaTemplate { type = type };
+
+        public MetaTemplate Len(int l)
+        {
+            len = l;
+            return this;
+        }
+
+        public byte[] ToBytes() 
+        {
+            var size = Marshal.SizeOf(this);
+            var arr = new byte[size];
+
+            var ptr = Marshal.AllocHGlobal(size);
+            Marshal.StructureToPtr(this, ptr, true);
+            Marshal.Copy(ptr, arr, 0, size);
+            Marshal.FreeHGlobal(ptr);
+            return arr;
+        }
+
+        public static MetaTemplate FromBytes(byte[] arr)
+        {
+            var str = new MetaTemplate();
+
+            var size = Marshal.SizeOf(str);
+            var ptr = Marshal.AllocHGlobal(size);
+
+            Marshal.Copy(arr, 0, ptr, size);
+
+            str = (MetaTemplate)Marshal.PtrToStructure(ptr, str.GetType());
+            Marshal.FreeHGlobal(ptr);
+            return str;
+        }
+    }
+}
---
+++ libs/Ancient.Runtime/emit/TemplateType.cs
@@ -0,0 +1,8 @@
+﻿namespace ancient.runtime.emit.@unsafe
+{
+    public enum TemplateType : short
+    {
+        STR = 1 << 1,
+        RND = 1 << 2
+    }
+}
