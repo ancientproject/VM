8596bfb71afd14bee61573d319e4101398f27246
Yuuki Wesp <ls-micro@ya.ru>
2019-07-19 02:05:31 +0300

    add metadata template and lpstr fixes

--- libs/Ancient.Runtime/instructions/lpstr.cs
+++ libs/Ancient.Runtime/instructions/lpstr.cs
@@ -1,8 +1,8 @@
 ﻿namespace ancient.runtime
 {
-    using System;
     using System.Collections.Generic;
     using System.Text;
+    using emit.@unsafe;
 
     public class lpstr : Instruction
     {
@@ -11,10 +11,7 @@
         public lpstr() : this("") {}
         public lpstr(string str) : base(IID.lpstr) => _str = str;
 
-        protected override void OnCompile()
-        {
-        }
-
+        protected override void OnCompile() { }
         public override bool HasMetadata() => true;
 
         protected internal override byte[] metadataBytes
@@ -23,7 +20,7 @@
             {
                 var len = Encoding.UTF8.GetByteCount(_str);
                 var bu = new List<byte>();
-                bu.AddRange(BitConverter.GetBytes(len));
+                bu.AddRange(MetaTemplate.By(TemplateType.STR).Len(len).ToBytes());
                 bu.AddRange(Encoding.UTF8.GetBytes(_str));
                 return bu.ToArray();
             }
