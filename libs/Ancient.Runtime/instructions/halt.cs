011ba264868d1ede9faf037046fa05e63c12691b
Yuuki Wesp <ls-micro@ya.ru>
2019-07-19 02:01:53 +0300

    impr halt

--- libs/Ancient.Runtime/instructions/halt.cs
+++ libs/Ancient.Runtime/instructions/halt.cs
@@ -1,11 +1,16 @@
 ﻿namespace ancient.runtime
 {
+    using System.Linq;
+    using System.Text;
+
     public class halt : Instruction
     {
         public halt() : base(IID.halt) { }
 
         protected override void OnCompile() 
-            => SetRegisters(0xE, 0xA, 0xD);
+            => Construct(0xE, 0xA, 0xD);
 
+        public override bool HasMetadata() => true;
+        protected internal override byte[] metadataBytes => Encoding.ASCII.GetBytes("@shutdown@").ToArray();
     }
 }
