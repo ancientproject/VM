ec2bb0dd73ace393657454196c68a7f6940c5714
Yuuki Wesp <ls-micro@ya.ru>
2019-07-21 17:12:48 +0300

    fix meta data in halt operaton

--- libs/Ancient.Runtime/instructions/halt.cs
+++ libs/Ancient.Runtime/instructions/halt.cs
@@ -1,7 +1,9 @@
 ﻿namespace ancient.runtime
 {
+    using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using emit.@unsafe;
 
     public class halt : Instruction
     {
@@ -11,6 +13,16 @@
             => Construct(0xE, 0xA, 0xD);
 
         public override bool HasMetadata() => true;
-        protected internal override byte[] metadataBytes => Encoding.ASCII.GetBytes("@shutdown@").ToArray();
+        protected internal override byte[] metadataBytes
+        {
+            get
+            {
+                var str = Encoding.ASCII.GetBytes("@shutdown@");
+                var bytes = new List<byte>();
+                bytes.AddRange(MetaTemplate.By(TemplateType.RND).Len(str.Length).ToBytes());
+                bytes.AddRange(str);
+                return bytes.ToArray();
+            }
+        }
     }
 }
