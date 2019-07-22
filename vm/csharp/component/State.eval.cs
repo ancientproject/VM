1d580e7ea016ed2109892a626394590684275483
Yuuki Wesp <ls-micro@ya.ru>
2019-07-22 16:23:35 +0300

    fix state eval  #13

--- vm/csharp/component/State.eval.cs
+++ vm/csharp/component/State.eval.cs
@@ -1,4 +1,4 @@
-﻿namespace vm.component
+namespace vm.component
 {
     using System;
     using System.Collections.Generic;
@@ -38,35 +38,35 @@
                     if(!ff) bus.cpu.halt(0xA9);
                     trace($"call :: [0xD0..0xE8]::0x{iid:X}");
                     var result = iid switch { // todo refactoring 
-                        0xD0 => f32i64 & Abs    (i64f32 & mem[r1]),
-                        0xD1 => f32i64 & Acos   (i64f32 & mem[r1]),
-                        0xD2 => f32i64 & Atan   (i64f32 & mem[r1]),
-                        0xD3 => f32i64 & Acosh  (i64f32 & mem[r1]),
-                        0xD4 => f32i64 & Atanh  (i64f32 & mem[r1]),
-                        0xD5 => f32i64 & Asin   (i64f32 & mem[r1]),
-                        0xD6 => f32i64 & Asinh  (i64f32 & mem[r1]),
-                        0xD7 => f32i64 & Cbrt   (i64f32 & mem[r1]),
-                        0xD8 => f32i64 & Ceiling(i64f32 & mem[r1]),
-                        0xD9 => f32i64 & Cos    (i64f32 & mem[r1]),
-                        0xDA => f32i64 & Cosh   (i64f32 & mem[r1]),
+                        0xD0 => f32u64 & Abs    (u64f32 & mem[r1]),
+                        0xD1 => f32u64 & Acos   (u64f32 & mem[r1]),
+                        0xD2 => f32u64 & Atan   (u64f32 & mem[r1]),
+                        0xD3 => f32u64 & Acosh  (u64f32 & mem[r1]),
+                        0xD4 => f32u64 & Atanh  (u64f32 & mem[r1]),
+                        0xD5 => f32u64 & Asin   (u64f32 & mem[r1]),
+                        0xD6 => f32u64 & Asinh  (u64f32 & mem[r1]),
+                        0xD7 => f32u64 & Cbrt   (u64f32 & mem[r1]),
+                        0xD8 => f32u64 & Ceiling(u64f32 & mem[r1]),
+                        0xD9 => f32u64 & Cos    (u64f32 & mem[r1]),
+                        0xDA => f32u64 & Cosh   (u64f32 & mem[r1]),
                        
-                        0xDB => f32i64 & Floor  (i64f32 & mem[r1]),
-                        0xDC => f32i64 & Exp    (i64f32 & mem[r1]),
-                        0xDD => f32i64 & Log    (i64f32 & mem[r1]),
-                        0xDE => f32i64 & Log10  (i64f32 & mem[r1]),
-                        0xDF => f32i64 & Tan    (i64f32 & mem[r1]),
-                        0xE0 => f32i64 & Tanh   (i64f32 & mem[r1]),
+                        0xDB => f32u64 & Floor  (u64f32 & mem[r1]),
+                        0xDC => f32u64 & Exp    (u64f32 & mem[r1]),
+                        0xDD => f32u64 & Log    (u64f32 & mem[r1]),
+                        0xDE => f32u64 & Log10  (u64f32 & mem[r1]),
+                        0xDF => f32u64 & Tan    (u64f32 & mem[r1]),
+                        0xE0 => f32u64 & Tanh   (u64f32 & mem[r1]),
                         
-                        0xE4 => f32i64 & Atan2  (i64f32 & mem[r1], i64f32 & mem[r2]),
-                        0xE5 => f32i64 & Min    (i64f32 & mem[r1], i64f32 & mem[r2]),
-                        0xE6 => f32i64 & Max    (i64f32 & mem[r1], i64f32 & mem[r2]),
+                        0xE4 => f32u64 & Atan2  (u64f32 & mem[r1], u64f32 & mem[r2]),
+                        0xE5 => f32u64 & Min    (u64f32 & mem[r1], u64f32 & mem[r2]),
+                        0xE6 => f32u64 & Max    (u64f32 & mem[r1], u64f32 & mem[r2]),
 
-                        0xE7 => f32i64 & Sin    (i64f32 & mem[r1]),
-                        0xE8 => f32i64 & Sinh   (i64f32 & mem[r1]),
+                        0xE7 => f32u64 & Sin    (u64f32 & mem[r1]),
+                        0xE8 => f32u64 & Sinh   (u64f32 & mem[r1]),
 
-                        0xE1 => f32i64 & Truncate(i64f32 & mem[r1]),
-                        0xE2 => f32i64 & BitDecrement(i64f32 & mem[r1]),
-                        0xE3 => f32i64 & BitIncrement(i64f32 & mem[r1]),
+                        0xE1 => f32u64 & Truncate(u64f32 & mem[r1]),
+                        0xE2 => f32u64 & BitDecrement(u64f32 & mem[r1]),
+                        0xE3 => f32u64 & BitIncrement(u64f32 & mem[r1]),
                         _ => throw new CorruptedMemoryException($"")
                     };
                     /* @stack-forward-flag */
@@ -89,14 +89,14 @@
                     _ = u2 switch
                     {
                         0x0 => mem[r1] = u1,
-                        _   => mem[r1] = i64 & ((u2 << 4) | u1)
+                        _   => mem[r1] = i64 | ((u2 << 4) | u1)
                     };
                     break;
                 case 0x1 when x2 == 0xA:
                     trace($"call :: ldx 0x{u1:X}, 0x{u2:X} -> 0x{r1:X}-0x{r2:X}");
-                    mem[((r1 << 4) | r2)] = i64 & ((u1 << 4) | u2);
+                    mem[((r1 << 4) | r2)] = i64 | ((u1 << 4) | u2);
                     break;
-                case 0x3: // 0x3120000
+                case 0x3: /* @swap */
                     trace($"call :: swap, 0x{r1:X}, 0x{r2:X}");
                     mem[r1] ^= mem[r2];
                     mem[r2] =  mem[r1] ^ mem[r2];
@@ -114,26 +114,26 @@
                     trace($"call :: rfd dev[0x{r1:X}], 0x{r2:X}");
                     stack.push(bus.Find(r1 & 0xFF).read(r2 & 0xFF));
                     break;
-                case 0x8 when u2 == 0xC: // 0x8F000C0
-                    trace($"call :: ref_t 0x{r1:X}");
-                    mem[r1] = i64 & pc;
+                case 0x8 when u2 == 0xC: /* @ref.t */
+                    trace($"call :: ref.t 0x{r1:X}");
+                    mem[r1] = pc;
                     break;
                 case 0xA0:
                     trace($"call :: orb '{r1}' times");
                     for (var i = pc + r1; pc != i;)
-                        stack.push(i64 & fetch());
+                        stack.push(fetch());
                     break;
                 case 0xA1:
                     trace($"call :: pull -> 0x{r1:X}");
                     mem[r1] = stack.pop();
                     break;
                 case 0xA5: /* @sig */
-                    stack.push(mem[r2] = i64 & pc);
+                    stack.push(mem[r2] = pc);
                     var frag = default(ulong?);
                     while (AcceptOpCode(frag) != 0xA6 /* @ret */)
                     {
                         var pc_r = stack.pop();
-                        frag = next(i64 | pc_r++);
+                        frag = next(pc_r++);
                         if (frag is null) 
                             bus.cpu.halt(0xA1);
                         stack.push(pc_r);
@@ -200,7 +200,7 @@
                 case 0xF when x2 == 0xF: /* @mvx */
                     var x = mem[u1].ToString();
 
-                    if (ff) x = (i64f32 & mem[u1]).ToString(CultureInfo.InvariantCulture);
+                    if (ff) x = (u64f32 & mem[u1]).ToString(CultureInfo.InvariantCulture);
 
                     static short[] cast(string str)
                     {
@@ -236,34 +236,34 @@
 
                 case 0x8 when u2 == 0xF && x1 == 0x0: /* @jump_t */
                     trace($"jump_t 0x{r1:X}");
-                    pc = (ulong)mem[r1];
+                    pc = mem[r1];
                     break;
                 case 0x8 when u2 == 0xF && x1 == 0x1: /* @jump_e  0x8FCD0F10 */
                     trace(mem[r2] >= mem[r3]
                         ? $"jump_e 0x{r1:X} -> 0x{r2:X} 0x{r3:X} -> apl"
                         : $"jump_e 0x{r1:X} -> 0x{r2:X} 0x{r3:X} -> skip");
                     if(mem[r2] >= mem[r3]) 
-                        pc = i64 | mem[r1];
+                        pc = mem[r1];
                     break;
                 case 0x8 when u2 == 0xF && x1 == 0x2: /* @jump_g */
                     trace(mem[r2] > mem[r3]
                         ? $"jump_g 0x{r1:X} -> 0x{r2:X} 0x{r3:X} -> apl"
                         : $"jump_g 0x{r1:X} -> 0x{r2:X} 0x{r3:X} -> skip");
                     if(mem[r2] > mem[r3])
-                        pc = (ulong)mem[r1];
+                        pc = mem[r1];
                     break;
                 case 0x8 when u2 == 0xF && x1 == 0x3: /* @jump_u */
                     trace(mem[r2] < mem[r3]
                         ? $"jump_u 0x{r1:X} -> 0x{r2:X} 0x{r3:X} -> apl"
                         : $"jump_u 0x{r1:X} -> 0x{r2:X} 0x{r3:X} -> skip");
                     if(mem[r2] < mem[r3])
-                        pc = (ulong)mem[r1];
+                        pc = mem[r1];
                     break;
                 case 0x8 when u2 == 0xF && x1 == 0x4: /* @jump_y */
                     trace(mem[r2] <= mem[r3]
                         ? $"jump_y 0x{r1:X} -> 0x{r2:X} 0x{r3:X} -> apl"
                         : $"jump_y 0x{r1:X} -> 0x{r2:X} 0x{r3:X} -> skip");
-                    if(mem[r2] <= mem[r3]) pc = (ulong)mem[r1];
+                    if(mem[r2] <= mem[r3]) pc = mem[r1];
                     break;
                 case 0x09: /* @jump */
                     pc = (ulong)((r1 << 4) | r2);
@@ -274,29 +274,29 @@
                 case 0xCA:
                     trace($"call :: sum 0x{r2:X}, 0x{r3:X}");
                     if (ff)
-                        mem[r1] = f32i64 & (i64f32 & mem[r2]) + (i64f32 & mem[r3]);
+                        mem[r1] = f32u64 & (u64f32 & mem[r2]) + (u64f32 & mem[r3]);
                     else
                         mem[r1] = mem[r2] + mem[r3];
                     break;
                 case 0xCB:
                     trace($"call :: sub 0x{r2:X}, 0x{r3:X}");
                     if (ff)
-                        mem[r1] = f32i64 & (i64f32 & mem[r2]) - (i64f32 & mem[r3]);
+                        mem[r1] = f32u64 & (u64f32 & mem[r2]) - (u64f32 & mem[r3]);
                     else
                         mem[r1] = mem[r2] - mem[r3];
                     break;
                 case 0xCC:
                     trace($"call :: div 0x{r2:X}, 0x{r3:X}");
                     _ = (mem[r3], ff) switch {
-                        (0x0, _    ) => bus.cpu.halt(0xC),
+                        (0x0, _    ) => (ulong)bus.cpu.halt(0xC),
                         (_  , false) => mem[r1] = mem[r2] / mem[r3],
-                        (_  , true ) => mem[r1] = f32i64 & (i64f32 & mem[r2]) / (i64f32 & mem[r3])
+                        (_  , true ) => mem[r1] = f32u64 & (u64f32 & mem[r2]) / (u64f32 & mem[r3])
                     };
                     break;
                 case 0xCD:
                     trace($"call :: mul 0x{r2:X}, 0x{r3:X}");
                     if (ff)
-                        mem[r1] = f32i64 & (i64f32 & mem[r2]) * (i64f32 & mem[r3]);
+                        mem[r1] = f32u64 & (u64f32 & mem[r2]) * (u64f32 & mem[r3]);
                     else
                         mem[r1] = mem[r2] * mem[r3];
                     break;
@@ -304,14 +304,14 @@
                 case 0xCE:
                     trace($"call :: pow 0x{r2:X}, 0x{r3:X}");
                     if (ff)
-                        mem[r1] = f32i64 & Pow(i64f32 & mem[r2], i64f32 & mem[r3]);
+                        mem[r1] = f32u64 & Pow(u64f32 & mem[r2], u64f32 & mem[r3]);
                     else
                         mem[r1] = (uint)Math.Pow(mem[r2], mem[r3]);
                     break;
                 case 0xCF:
                     trace($"call :: sqrt 0x{r2:X}");
                     if (ff)
-                        mem[r1] = f32i64 & Sqrt(i64f32 & mem[r2]);
+                        mem[r1] = f32u64 & Sqrt(u64f32 & mem[r2]);
                     else
                         mem[r1] = (uint)Math.Sqrt(mem[r2]);
                     break;
@@ -371,7 +371,7 @@
             {
                 var result = info.method.Invoke(null, new object[0]);
                 if(!info.IsVoid())
-                    stack.push(result.To<long>());
+                    stack.push(result.To<ulong>());
             }
             catch (Exception e)
             {
