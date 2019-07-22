83687aede892c1e795afee49cb5c0a6fd1634787
Yuuki Wesp <ls-micro@ya.ru>
2019-07-22 17:09:18 +0300

    add support type in unlock

--- libs/Ancient.Runtime/instructions/unlock.cs
+++ libs/Ancient.Runtime/instructions/unlock.cs
@@ -1,17 +1,30 @@
 ﻿namespace ancient.runtime
 {
+    using System;
+    using System.Text;
     using emit.@unsafe;
 
     public class unlock : Instruction
     {
-        private readonly ushort _handle;
+        private readonly byte _handle;
+        private readonly string _type;
 
-        public unlock(ushort handle) : base(IID.unlock) => _handle = handle;
+        public unlock(byte handle, string type) : base(IID.unlock)
+        {
+            _handle = handle;
+            _type = type;
+            if(_type.Length > 3)
+                throw new InvalidOperationException($"Type len cannot more 3 symbols.");
+        }
 
         protected override void OnCompile()
         {
-            var (r1, r2, r3, u1) = new d16u(_handle);
-            Construct(r1, r2, r3, u1);
+            var bytes = Encoding.ASCII.GetBytes(_type);
+            var (r1, r2) = new d8u(_handle);
+            var (r3, u1) = new d8u(bytes[0]);
+            var (u2, x1) = new d8u(bytes[1]);
+            var (x2, x3) = new d8u(bytes[2]);
+            Construct(r1, r2, r3, u1, u2, x1, x2, x3);
         }
     }
 }
