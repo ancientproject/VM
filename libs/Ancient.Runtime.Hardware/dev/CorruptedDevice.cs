a21c0f88bd589154b8745a1807dc988f32aff1d5
Yuuki Wesp <ls-micro@ya.ru>
2019-07-22 16:04:10 +0300

    bump version and set target arch x64 in all project

--- libs/Ancient.Runtime.Hardware/dev/CorruptedDevice.cs
+++ libs/Ancient.Runtime.Hardware/dev/CorruptedDevice.cs
@@ -11,7 +11,7 @@
         public override void write(long address, long data) 
             => throw new CorruptedMemoryException($"Instruction at address 0x{_cpu.State.curAddr:X4} accessed memory 0x{address:X}. Memory could not be write.");
 
-        public override long read(long address) 
+        public override ulong read(long address) 
             => throw new CorruptedMemoryException($"Instruction at address 0x{_cpu.State.curAddr:X4} accessed memory 0x{address:X}. Memory could not be read.");
     }
 }
