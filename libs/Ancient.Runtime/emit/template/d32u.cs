d21441e440b9a116d976165f1132467394a79b1a
Yuuki Wesp <ls-micro@ya.ru>
2019-07-22 17:08:54 +0300

    add implicit operator to d32u

--- libs/Ancient.Runtime/emit/template/d32u.cs
+++ libs/Ancient.Runtime/emit/template/d32u.cs
@@ -14,6 +14,25 @@
             n6 = (byte)((_value & 0x00000F00) >> shift());
             n7 = (byte)((_value & 0x000000F0) >> shift());
             n8 = (byte)((_value & 0x0000000F) >> shift());
+            resetShifter();
         }
+        public (byte n1, byte n2, byte n3, byte n4, byte n5, byte n6, byte n7, byte n8) Deconstruct()
+        {
+            var (n1, n2, n3, n4, n5, n6, n7, n8) = this;
+            return (n1, n2, n3, n4, n5, n6, n7, n8);
+        }
+        public d32u Construct(in byte n1, in byte n2, in byte n3, in byte n4, in byte n5, in byte n6, in byte n7, in byte n8)
+        {
+            resetShifter();
+            this._value = (ushort) ((n1 << shift()) | (n2 << shift()) | (n3 << shift()) | (n4 << shift())
+                                    | (n5 << shift())| (n6 << shift())| (n7 << shift())| (n8 << shift()));
+            return this;
+        }
+
+        public static implicit operator (byte n1, byte n2, byte n3, byte n4, byte n5, byte n6, byte n7, byte n8)(d32u u) => u.Deconstruct();
+        public static implicit operator d32u((byte n1, byte n2, byte n3, byte n4, byte n5, byte n6, byte n7, byte n8) u) => new d32u(0).Construct(u.n1, u.n2, u.n3, u.n4, u.n5, u.n6, u.n7, u.n8);
+
+        public static implicit operator uint(d32u u) => u.Value;
+        public static implicit operator int(d32u u) => (int)u.Value;
     }
 }
