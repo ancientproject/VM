fb351df8bfd43d70c5da0540e8deb88f628d6cd3
Yuuki Wesp <ls-micro@ya.ru>
2020-04-05 07:52:15 +0300

    add class of readonly instruction

---
+++ libs/Ancient.Runtime/instructions/readonly.cs
@@ -0,0 +1,13 @@
+﻿namespace ancient.runtime
+{
+    public class @readonly : Instruction
+    {
+        private readonly byte _cell;
+
+        public @readonly(byte cell) : base(IID.@readonly) 
+            => _cell = cell;
+
+        protected override void OnCompile()
+            => Construct(_cell);
+    }
+}
