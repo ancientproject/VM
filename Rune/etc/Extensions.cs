9cb0d560497204d71605ac941ec2a36b25ea3f5c
Yuuki Wesp <ls-micro@ya.ru>
2020-04-13 09:02:58 +0300

    add extensions for readtoend

---
+++ Rune/etc/Extensions.cs
@@ -0,0 +1,14 @@
+﻿namespace rune.etc
+{
+    using System.IO;
+
+    public static class Extensions
+    {
+        public static string ReadToEnd(this FileInfo info)
+        {
+            if(!info.Exists)
+                throw new FileNotFoundException($"'{info.FullName}' not exist.");
+            return info.OpenText().ReadToEnd();
+        }
+    }
+}
