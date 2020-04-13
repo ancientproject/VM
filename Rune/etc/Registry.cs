4f70e81a783b99e3b4dd01737f83f1019bbceb33
Yuuki Wesp <ls-micro@ya.ru>
2020-04-13 09:04:30 +0300

    extract file for github registry

--- Rune/etc/Registry.cs
+++ Rune/etc/Registry.cs
@@ -1,14 +1,10 @@
 ﻿namespace rune.etc
 {
     using System;
-    using System.Drawing;
-    using System.Linq;
     using System.Reflection;
     using System.Text.RegularExpressions;
-    using Ancient.ProjectSystem;
-    using Flurl.Http;
-    using Newtonsoft.Json;
-    using Octokit;
+    using System.Threading.Tasks;
+    using registry;
 
     internal enum RegistryType
     {
@@ -36,81 +32,16 @@
             {
                 case RegistryType.github:
                     return new GitHubOrgRegistry(url);
-
+                case RegistryType.pkg:
+                    return new RunicRegistry();
                 default:
                     throw new NotSupportedException();
             }
         }
     }
-
-    public class GitHubOrgRegistry : IRegistry
-    {
-        private readonly string _url;
-        private readonly GitHubClient client;
-        private string owner => new Regex(@"https\:\/\/github\.com\/(?<id>\w+)").Match(_url).Groups["id"].Value;
-
-        public GitHubOrgRegistry(string url)
-        {
-            _url = url;
-            client = new GitHubClient(new ProductHeaderValue("0xF6"));
-        }
-
-        public bool Exist(string id)
-        {
-            Console.Write($"{":thought_balloon:".Emoji()} GET '{owner}/{id}'...".Color(Color.DimGray));
-
-            var result = client.Repository.GetAllForUser(owner).Result.FirstOrDefault(x => x.Name == id) != null;
-
-            Console.WriteLine(result ? $".. OK".Color(Color.DimGray) : $".. 404".Color(Color.DimGray));
-            return result;
-        }
-
-        public Assembly Put(string id, out byte[] assembly)
-        {
-            var repo = client.Repository.GetAllForUser(owner).Result.FirstOrDefault(x => x.Name == id);
-            assembly = Array.Empty<byte>();
-            if (repo is null)
-            {
-                Console.WriteLine($"{":thought_balloon:".Emoji()} [github] {"package".Nier()} '{id}' not found in '{owner}'".Color(Color.Orange));
-                return null;
-            }
-
-            var content = client.Repository.Content.GetAllContents(repo.Id).Result;
-
-            if (content is null)
-            {
-                Console.WriteLine($"{":thought_balloon:".Emoji()} [github] {"package".Nier()} '{owner}/{id}' failed fetch files.".Color(Color.Orange));
-                return null;
-            }
-
-            var box = content.FirstOrDefault(x => x.Name == "box.json");
-
-            if (box is null)
-            {
-                Console.WriteLine($"{":thought_balloon:".Emoji()} [github] '{owner}/{id}' is not a {"package".Nier()}.".Color(Color.Orange));
-                return null;
-            }
-            Console.Write($"{":thought_balloon:".Emoji()} PUT '{owner}/{id}'...".Color(Color.DimGray));
-            var temp = box.DownloadUrl.WithTimeout(10).GetStringAsync().Result;
-
-            var ancientBox = JsonConvert.DeserializeObject<AncientPackageBox>(temp);
-
-            var file = content.FirstOrDefault(x => x.Name == ancientBox.files.First());
-            Console.WriteLine($".. OK".Color(Color.DimGray));
-            if (file is null)
-            {
-                Console.WriteLine($"{":thought_balloon:".Emoji()} [github] {"package".Nier()} '{owner}/{id}' box.json bad.".Color(Color.Orange));
-                return null;
-            }
-            assembly = CSharpCompile.Build(id, file.DownloadUrl.WithTimeout(10).GetStringAsync().Result);
-            return Assembly.Load(assembly);
-        }
-    }
-
     public interface IRegistry
     {
-        bool Exist(string id);
-
-        Assembly Put(string id, out byte[] assemblyBytes);
+        Task<bool> Exist(string id);
+        Task<(Assembly assembly, byte[] raw)> Fetch(string id);
     }
 }
---
+++ Rune/etc/registry/GitHubOrgRegistry.cs
@@ -0,0 +1,77 @@
+﻿namespace rune.etc.registry
+{
+    using System;
+    using System.Drawing;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+    using Ancient.ProjectSystem;
+    using Flurl.Http;
+    using Newtonsoft.Json;
+    using Octokit;
+
+    public class GitHubOrgRegistry : IRegistry
+    {
+        private readonly string _url;
+        private readonly GitHubClient client;
+        private string owner => new Regex(@"https\:\/\/github\.com\/(?<id>\w+)").Match(_url).Groups["id"].Value;
+
+        public GitHubOrgRegistry(string url)
+        {
+            _url = url;
+            client = new GitHubClient(new ProductHeaderValue("0xF6"));
+        }
+
+        public async Task<bool> Exist(string id)
+        {
+            Console.Write($"{":thought_balloon:".Emoji()} GET '{owner}/{id}'...".Color(Color.DimGray));
+
+            var repos = await client.Repository.GetAllForUser(owner);
+            var result = repos.Any(x => x.Name == id);
+
+            Console.WriteLine(result ? $".. OK".Color(Color.DimGray) : $".. 404".Color(Color.DimGray));
+            return result;
+        }
+
+        public async Task<(Assembly assembly, byte[] raw)> Fetch(string id)
+        {
+            var repo = (await client.Repository.GetAllForUser(owner)).FirstOrDefault(x => x.Name == id);
+            if (repo is null)
+            {
+                Console.WriteLine($"{":thought_balloon:".Emoji()} [github] {"package".Nier()} '{id}' not found in '{owner}'".Color(Color.Orange));
+                return default;
+            }
+
+            var content = await client.Repository.Content.GetAllContents(repo.Id);
+
+            if (content is null)
+            {
+                Console.WriteLine($"{":thought_balloon:".Emoji()} [github] {"package".Nier()} '{owner}/{id}' failed fetch files.".Color(Color.Orange));
+                return default;
+            }
+
+            var box = content.FirstOrDefault(x => x.Name == "box.json");
+
+            if (box is null)
+            {
+                Console.WriteLine($"{":thought_balloon:".Emoji()} [github] '{owner}/{id}' is not a {"package".Nier()}.".Color(Color.Orange));
+                return default;
+            }
+            Console.Write($"{":thought_balloon:".Emoji()} PUT '{owner}/{id}'...".Color(Color.DimGray));
+            var temp = await box.DownloadUrl.WithTimeout(10).GetStringAsync();
+
+            var ancientBox = JsonConvert.DeserializeObject<AncientPackageBox>(temp);
+
+            var file = content.FirstOrDefault(x => x.Name == ancientBox.files.First());
+            Console.WriteLine($".. OK".Color(Color.DimGray));
+            if (file is null)
+            {
+                Console.WriteLine($"{":thought_balloon:".Emoji()} [github] {"package".Nier()} '{owner}/{id}' box.json bad.".Color(Color.Orange));
+                return default;
+            }
+            var raw = CSharpCompile.Build(id, await file.DownloadUrl.WithTimeout(10).GetStringAsync());
+            return (Assembly.Load(raw), raw);
+        }
+    }
+}
