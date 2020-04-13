04f2edcdcdb89a38b8df49e6b5b7116c4cb2f5ca
Yuuki Wesp <ls-micro@ya.ru>
2020-04-13 09:02:49 +0300

    add dirs

---
+++ Rune/etc/Dirs.cs
@@ -0,0 +1,20 @@
+﻿namespace rune.etc
+{
+    using System;
+    using System.IO;
+
+    public static class Dirs
+    {
+        public static DirectoryInfo RootFolder =>
+            new DirectoryInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".rune"));
+        public static FileInfo ConfigFile =>
+            new FileInfo(Path.Combine(RootFolder.FullName, "@.ini"));
+
+
+        public static void Ensure()
+        {
+            if(!RootFolder.Exists)
+                RootFolder.Create();
+        }
+    }
+}
