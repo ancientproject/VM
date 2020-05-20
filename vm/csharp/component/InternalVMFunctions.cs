fd21dc011a29c6d5a44042f35712f7b2096e0260
Yuuki Wesp <ls-micro@ya.ru>
2020-05-20 11:24:42 +0300

    add internal functions vm; add display callstack

---
+++ vm/csharp/component/InternalVMFunctions.cs
@@ -0,0 +1,32 @@
+﻿namespace vm.component
+{
+    using System.Text;
+    using ancient.runtime.emit.sys;
+    using static System.Console;
+
+    public static class InternalVMFunctions
+    {
+        private static Bus _bus { get; set; }
+
+
+        public static void Setup(Bus bus)
+        {
+            _bus = bus;
+            Module.Global.Add("sys->DumpCallStack()", typeof(InternalVMFunctions).GetMethod("DumpCallStack"));
+        }
+
+
+
+        public static void DumpCallStack()
+        {
+            var callStack = _bus.State.CallStack;
+            var frames = callStack.GetFrames();
+            var text = new StringBuilder();
+
+            foreach (var frame in frames) 
+                WriteLine($"at {frame}");
+
+            WriteLine(text.ToString());
+        }
+    }
+}
